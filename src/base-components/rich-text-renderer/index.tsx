da143c06fba99022abed7d7aa381247f0d21f3a5
mgruber <m.gruber@cyclus.digital>
2021-10-11 18:37:09 +0200

    chore: update imports of renderer
    chore: update imports of renderer

--- src/base-components/rich-text-renderer/index.tsx
+++ src/base-components/rich-text-renderer/index.tsx
@@ -1,5 +1,5 @@
-export { default } from "./renderer-element";
-export { documentRenderer } from "./renderer-element";
-export type { ElementType } from "./renderer-element";
-export { DEFAULT_BLOCK_MAP } from "./renderer-element";
+export { default } from "./renderer";
+export { documentRenderer } from "./renderer";
+export type { ElementType } from "./renderer";
+export { DEFAULT_BLOCK_MAP } from "./renderer";
 export { RichTextRenderer } from "./RichTextRenderer";
