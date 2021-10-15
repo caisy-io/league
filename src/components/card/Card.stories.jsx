99b97fa9995f3c5b98c5a4cc34be5d706273c041
Andrijana Radic <a.radic@cyclus.digital>
2021-10-15 13:31:22 +0200

    feat: added card stories

---
+++ src/components/card/Card.stories.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import { Card, SCardBody, SCardHeaderTitle } from "../..";
+
+const SDemo = styled.div`
+  margin: 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 20;
+  align-items: center;
+`;
+
+const SDemoCardBody = styled.div`
+  min-height: 300px;
+  min-width: 300px;
+`;
+
+function CardDemo() {
+    return (
+        <SDemo>
+          <Card header={<SCardHeaderTitle>Supercard</SCardHeaderTitle>}>
+            <SCardBody>
+              <SDemoCardBody>Some card body</SDemoCardBody>
+            </SCardBody>
+          </Card>
+        </SDemo>
+      );
+}
+
+export default {
+  title: 'Components/Card',
+  component: CardDemo,
+};
+const Template = (args) => <CardDemo {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
+
