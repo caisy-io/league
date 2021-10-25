e6e163b2275f12d75ff865a72e7801f726e994be
Nicolas Shiken <nicolasshiken@gmail.com>
2021-10-25 19:32:41 -0300

    feat: we now use rem instead of px and generate font-variables in SC format

---
+++ src/constants/styles/design-tokens/fonts/CSSFonts.tsx
@@ -0,0 +1,30 @@
+import { css } from "styled-components" 
+
+export const CSSFonts = css `
+  :root {
+    --font-families: Inter;
+    --font-families: IBM Plex Mono;
+    --line-heights: 1.5rem;
+    --line-heights: 1.25rem;
+    --line-heights: 1rem;
+    --line-heights: 0.875rem;
+    --line-heights: 0.75rem;
+    --font-weights: Bold;
+    --font-weights: Semi Bold;
+    --font-weights: Medium;
+    --font-weights: Regular;
+    --font-weights: Text;
+    --font-sizes: 0.5rem;
+    --font-sizes: 0.5625rem;
+    --font-sizes: 0.6875rem;
+    --font-sizes: 0.75rem;
+    --font-sizes: 0.875rem;
+    --font-sizes: 1rem;
+    --font-sizes: 1.125rem;
+    --letter-spacing: 0%;
+    --letter-spacing: -1%;
+    --letter-spacing: 6%;
+    --letter-spacing: 8%;
+    --paragraph-spacing: 0;
+  }
+`;
