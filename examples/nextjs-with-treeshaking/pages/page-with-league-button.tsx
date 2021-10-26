2ee610d729e89460c69512b2fc5a7b003f23b099
Nicolas Shiken <nicolasshiken@gmail.com>
2021-10-25 21:12:36 -0300

    chore: refactored components index to work with treeshaking

---
+++ examples/nextjs-with-treeshaking/pages/page-with-league-button.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@caisy/league";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>nextjs with treeshaking</title>
+        <meta name="description" content="nextjs with treeshaking @caisy/league" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>nextjs with treeshaking</h1>
+
+        <Button size="large">Button</Button>
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/form/FormField.tsx
+++ src/components/form/FormField.tsx
@@ -4,18 +4,18 @@ import { FormErrors } from "./FormErrors";
 import { FormFieldWrapper, IRule } from "./FormFieldWrapper";
 import { SFormField } from "./styles/SFormField";
 
-export interface IError {
+interface IError {
   name: string;
   message: string;
 }
 
-export interface IField {
+interface IField {
   value: any;
   errors: IError[];
   rules: any;
 }
 
-export interface IControl {
+interface IControl {
   onFieldChange: (fieldName: string, value: any) => void;
   formState: any;
   registerField: (fieldName: string, field: IField) => void;
