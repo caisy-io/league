26a04da10b3020b4ff7c6004b764d2c402aa03c0
Andrijana Radic <a.radic@cyclus.digital>
2021-11-02 08:44:21 +0100

    refactor: added license

--- src/components/resizable/Resizable.resizers.tsx
+++ src/components/resizable/Resizable.resizers.tsx
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2020, Amdocs Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, {useCallback, memo} from 'react';
 import { Movable } from 'webrix/components';
 
@@ -12,6 +28,7 @@ const Resizer = ({direction, onBeginResize, onResize, onEndResize}: {direction?:
 );
 
 const Left = memo(props => {
+    //@ts-ignore
     const { onResize } = props;
     const handleOnResize = useCallback(({dx, cx}) => (
         onResize({
@@ -27,6 +44,7 @@ const Left = memo(props => {
 Left.displayName = 'Resizable.Resizer.Left';
 
 const Right = memo(props => {
+    //@ts-ignore
     const { onResize } = props;
     const handleOnResize = useCallback(({dx, cx}) => (
         onResize({
--- src/components/resizable/Resizable.tsx
+++ src/components/resizable/Resizable.tsx
@@ -1,7 +1,23 @@
 
+/**
+ * Copyright (c) 2020, Amdocs Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { memo } from 'react';
 import { SResizable } from './styles/SResizable';
-import { propTypes, defaultProps } from './Resizable.props';
+import { defaultProps } from './Resizable.props';
 
 export const Resizable = ({onBeginResize, onResize, onEndResize, children}) => (
     <SResizable>
@@ -16,7 +32,6 @@ export const Resizable = ({onBeginResize, onResize, onEndResize, children}) => (
 );
 
 Resizable.displayName = 'Resizable';
-Resizable.propTypes = propTypes; 
 Resizable.defaultProps = defaultProps;
 
 export default memo(Resizable);
