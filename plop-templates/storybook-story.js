fba5093999952b1d9b4ab3fcd094828943f49751
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-11-03 16:50:57 +0000

    extend plop templates with template for storybook story

---
+++ plop-templates/storybook-story.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { {{ properCase name }} } from './{{ properCase name }}';
+
+export default {
+  title: `Components/{{ properCase name }}`,
+  component: {{ properCase name }}Demo,
+  argTypes: {
+    // example argument type
+    activated: {
+      description: "Changes the state of the component to Activated",
+      control: { type: "boolean" },
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+  },
+};
+
+function {{properCase name}}Demo({ content, ...args }) {
+  <{{properCase name}}>
+      {content}
+    </{{properCase name}}>
+}
+
+const Template = (args) => < {{ properCase name }} {...args } />;
+
+export const Default = Template.bind({});
+Default.args = {
+
+};
