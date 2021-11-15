9612114549f962a518cda03ebf8974455a5db8d1
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-11-15 09:29:21 +0000

    updated figma link

--- src/components/close-button/CloseButton.stories.jsx
+++ src/components/close-button/CloseButton.stories.jsx
@@ -6,7 +6,7 @@ export default {
   parameters: {
     design: {
       type: "figma",
-      url: "https://www.figma.com/file/1hz5D4Q7pj5w0YrRw7hXbu/UI-Kit?node-id=1094%3A18210",
+      url: "https://www.figma.com/file/1hz5D4Q7pj5w0YrRw7hXbu/UI-Kit?node-id=1112%3A16949",
     },
   },
   component: CloseButtonDemo,
