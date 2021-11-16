987677c9576d5d516a16dac4c1a7350dd0a09f30
Diego Martinez <diego@MacBook-Pro.local>
2021-11-16 14:42:06 -0300

    Merged dev back

--- src/components/avatar/styles/SAvatar.tsx
+++ src/components/avatar/styles/SAvatar.tsx
@@ -1,25 +1,25 @@
 import styled, { css } from "styled-components";
 import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles/mediaquerys";
-import { ProgressiveHighLight } from "../../../constants/styles/fonts";
+import { CSSProgressiveHighLight } from "../../../constants/styles/fonts";
 
 interface ISAvatar {
   size?: 16 | 20 | 24 | 28 | 32 | 40;
 }
 
 const Bronze = css<ISAvatar>`
-  ${ProgressiveHighLight};
+  ${CSSProgressiveHighLight};
   background-color: var(--ui-03);
   border-radius: 4px;
   color: var(--text-04);
-  font-size: ${(props) => props.size ? (props.size / 2) - 2 : 14}px;
-  height: ${(props) => props.size || 32 }px;
-  line-height: ${(props) => props.size || 32 }px;
-  letter-spacing: 0.06em; 
+  font-size: ${(props) => (props.size ? props.size / 2 - 2 : 14)}px;
+  height: ${(props) => props.size || 32}px;
+  line-height: ${(props) => props.size || 32}px;
+  letter-spacing: 0.06em;
   overflow: hidden;
   position: relative;
   text-align: center;
   text-transform: uppercase;
-  width: ${(props) => props.size || 32 }px;
+  width: ${(props) => props.size || 32}px;
 `;
 
 const Silver = css``;
