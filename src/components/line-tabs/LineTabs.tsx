909f052c090d482ed86e3cf512e287f24a5729f0
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-11-16 10:48:50 +0000

    update tab component to use new line tab component

--- src/components/line-tabs/LineTabs.tsx
+++ src/components/line-tabs/LineTabs.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { SLineTabs } from './styles/SLineTabs';
 
 export interface ILineTabsProps {
   hover?: boolean;
   activated?: boolean;
   title?: string | undefined;
+  onClick: () => void;
 }
 
 export const LineTabs: React.FC<ILineTabsProps> = ({ ...props }) => {
