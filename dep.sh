69f0efb57b2309fe1564d951caef4fd23055a8a0
mgruber <m.gruber@cyclus.digital>
2021-11-16 13:56:21 +0100

    ci: update the ci pipeline with right
    ci: update the ci pipeline with right branch name 🐳

--- dep.sh
+++ dep.sh
@@ -2,4 +2,4 @@ yarn global add vercel
 
 DEPLOYMENT_URL=$(vercel --token=$VERCEL_TOKEN --confirm --scope=caisy --no-clipboard --name="$VERCEL_NAME" ) && \
 curl   --header "Authorization: Basic $INTERNAL_AUTOMATION_AUTH_TOKEN" \
-https://dev.caisy.io/api/internal-automation/trigger/linear/comment-preview-link\?branch\=search-bar-operator-badge-select-cy-150a-asdf\&body\=Preview-Link:%20$DEPLOYMENT_URL || true
+https://dev.caisy.io/api/internal-automation/trigger/linear/comment-preview-link\?branch\=${GITHUB_REF##*/}\&body\=Preview-Link:%20$DEPLOYMENT_URL || true
