0140659f6ccd82135e9eed6dec6f6470cffac7f0
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-11-18 16:04:05 +0000

    extract css style snippet into its own file

--- src/components/scrollbar/styles/SScrollbar.tsx
+++ src/components/scrollbar/styles/SScrollbar.tsx
@@ -1,22 +1,10 @@
 import styled, { css } from 'styled-components';
 import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from '../../../constants/styles/mediaquerys';
+import { CSSScrollbar } from './CSSScrollbar';
 
 const Bronze = css`
-overflow-y: scroll;
-::-webkit-scrollbar {
-  background-color: #FFFF;
-  border-right: 1px solid #E8EFF3;
-  border-left: 1px solid #E8EFF3;
-  width: 0.75rem;
-  box-sizing: border-box;
-}
-::-webkit-scrollbar-thumb  {
-  background: var(--interactional-secondary-08);
-  border: 3px solid rgba(0, 0, 0, 0);
-  background-clip: padding-box;
-  -webkit-border-radius: 49px;
-  background-color: rgba(0, 0, 0, 0.15);
- }
+  overflow-y: scroll;
+  ${CSSScrollbar};
 `;
 
 const Silver = css``;
