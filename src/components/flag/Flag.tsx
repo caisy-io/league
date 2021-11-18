b908f6dbf190a7564ce78e1b8be8e3bdd3e598ec
Cristian Martinez <cristian.pablo3403@gmail.com>
2021-11-18 11:27:43 -0300

    Fixed comments

--- src/components/flag/Flag.tsx
+++ src/components/flag/Flag.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from "react";
 import * as Flags from "./flags"
 import { SFlags } from "./styles/SFlags";
+import { IFlag } from "./IFlag";
 
-const Flag: FC<any> = ({ countryCode, size }) => {
-  const Flag = Flags[countryCode]
+const Flag: FC<IFlag> = ({ countryCode, size }) => {
+  const Flag = countryCode && Flags[countryCode];
   return (
     <SFlags size={size}>
       <Flag/>
     </SFlags>
   )
-}
+};
 
 export default Flag;
