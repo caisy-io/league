b0a2054df7b40bc1098f831aed000f19d8965e41
Nicolas Shiken <nicolasshiken@gmail.com>
2021-11-24 19:53:51 -0300

    CY-58-Form-InputField-Simple-Input (#16)
    * Revert "Merge pull request #7 from caisy-io/CY-59-refactor-components-exports-to-work-with-tree-shaking"
    
    This reverts commit ca1e425dd32f36b2f236d338f43c37fcf7d7552e, reversing
    changes made to dbc2623cc80be013fd3073142e7748e63a0aec42.
    
    * feat: added SimpleInput component and story
    
    * feat: added required indicator
    
    * fix: removed label property
    
    * Update FormField.tsx
    
    * added back removed commit
    
    * Update FormField.tsx
    
    * feat: added WithError and WithLabel components
    
    * feat: added FieldContext and WithAddon component
    
    * Update WithLabel.stories.jsx
    
    * Update WithLabel.stories.jsx
    
    * fix: moved input story and fixed some errors
    
    * fix: fixed FieldContextState to return children instead of cloned ones
    
    * feat: added withError HOC
    
    * feat: we now use  HOC for the addon components
    
    * chore: removed unnecessary code and fixed story
    
    * handler now use useCallback and badge is conditionally rendered
    
    * Update SimpleInput.tsx
    
    * Update SimpleInput.tsx

---
+++ src/components/input-fields/simple-input/SimpleInput.tsx
@@ -0,0 +1,98 @@
+import React, { InputHTMLAttributes } from "react";
+import { SErrorMessage } from "../styles/SErrorMessage";
+import { SLabel } from "../styles/SLabel";
+import { SSimpleInput } from "./styles/SSimpleInput";
+import { SSimpleInputRequiredIndicator } from "./styles/SSimpleInputRequiredIndicator";
+import { SSimpleInputRequiredIndicatorContainer } from "./styles/SSimpleInputRequiredIndicatorContainer";
+import { SSimpleInputTextWidth } from "./styles/SSimpleInputTextWidth";
+import { SSimpleInputWrapper } from "./styles/SSimpleInputWrapper";
+
+type TSimpleInputState = "success" | "error";
+
+interface ISimpleInput extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  errors?: string[];
+  state?: TSimpleInputState;
+  translationBadge?: JSX.Element;
+  required?: boolean;
+  onChange: any;
+  value: any;
+}
+
+export const SimpleInput: React.FC<ISimpleInput> = ({ state, required, children, value, onChange, ...props }) => {
+  const [active, setActive] = React.useState(false);
+  const [inputLength, setInputLength] = React.useState(value?.toString().length);
+
+  const inputRef = React.useRef<any>();
+  const spanRef = React.useRef<any>();
+
+  const handleClick = React.useCallback(() => {
+    inputRef.current.focus();
+  }, [inputRef.current?.focus]);
+
+  const resizeInput = React.useCallback(() => {
+    if (!inputRef.current.value && props.placeholder) {
+      spanRef.current.innerText = props.placeholder;
+    } else {
+      spanRef.current.innerText = inputRef.current.value;
+    }
+    let width = spanRef.current.scrollWidth;
+    if (inputRef.current.value[inputRef.current.value.toString().length - 1] === " ") {
+      width += 3;
+    }
+    setInputLength(width + 1);
+  }, [setInputLength]);
+
+  React.useEffect(() => {
+    resizeInput();
+  }, [value, props.placeholder]);
+
+  const handleFocus = React.useCallback(
+    (e) => {
+      setActive(true);
+      props.onFocus?.(e);
+    },
+    [setActive, props.onFocus],
+  );
+
+  const handleBlur = React.useCallback(
+    (e) => {
+      setActive(false);
+      props.onBlur?.(e);
+    },
+    [setActive, props.onBlur],
+  );
+
+  return (
+    <SSimpleInputWrapper active={active} state={state} onClick={handleClick}>
+      <SSimpleInputTextWidth ref={spanRef} />
+      {props.label && (
+        <SSimpleInputRequiredIndicatorContainer>
+          {required && <SSimpleInputRequiredIndicator />}
+          <SLabel>{props.label}</SLabel>
+        </SSimpleInputRequiredIndicatorContainer>
+      )}
+
+      {props.errors && props.errors.map((error) => <SErrorMessage>{error}</SErrorMessage>)}
+
+      {props.translationBadge && props.translationBadge}
+
+      <SSimpleInputRequiredIndicatorContainer>
+        {required && !props.label && <SSimpleInputRequiredIndicator />}
+        <SSimpleInput
+          onChange={(e) => {
+            resizeInput();
+            onChange(e);
+          }}
+          width={inputLength}
+          required={required}
+          ref={inputRef}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          value={value || ""}
+          {...props}
+        />
+      </SSimpleInputRequiredIndicatorContainer>
+    </SSimpleInputWrapper>
+  );
+};
