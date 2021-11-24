2a289b739e77d252b915a900789f5c158eab7695
Moritz Gruber <m.gruber@cyclus.digital>
2021-11-24 13:37:54 +0100

    fix: the script and move it to
    fix: the script and move it to file 🐛

---
+++ run-sb-exit.sh
@@ -0,0 +1,18 @@
+
+
+function sb_success () {
+    echo "Storybook build successful."
+    exit 0
+}
+
+function sb_failed () {
+     grep -vwE "webpack.Progress" sb-out.txt 
+     echo "-- ERROR --"
+     echo "-- the storybook output does contain \"failed\" -> see above"
+     echo "--"
+     exit 1
+}
+
+yarn build-storybook --loglevel warn 2> ./sb-out.txt 
+
+grep -q "failed" ./sb-out.txt; [ $? -eq 0 ] && sb_failed || sb_success
