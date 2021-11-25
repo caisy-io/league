04b0f58de0a25fd0160df1e261acb9486fa68562
Andrijana Radic <44426503+radicaa@users.noreply.github.com>
2021-11-25 15:22:55 +0100

    CY-170-solid-label (#51)
    * feat: started adding solid labels, styles are not done yet
    
    * refactor: added styles
    
    * fix: error
    
    * refactor: CSSColors, Label styles
    
    * feat: added to comp index and added index to solidLabel
    
    * fix: styles should not only apply to p and span

---
+++ src/components/solidLabel/styles/SSolidLabel.tsx
@@ -0,0 +1,104 @@
+import styled, { css } from "styled-components";
+import { CSSProgressiveCaption01Semibold } from "../../../constants/styles/fonts";
+import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles/mediaquerys";
+import { ISolidLabelProps, ISolidLabelType } from "../SolidLabel";
+
+const CSSSolidLabelBase = css`
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 6px 8px;
+  border-radius: 4px;
+  border: none;
+  
+  > * {
+    ${CSSProgressiveCaption01Semibold}
+    line-height: normal;
+    text-align: center;
+  }
+`;
+
+const CSSDefault = css`
+  ${CSSSolidLabelBase}
+  background: var(--ui-02);
+  color: var(--text-suppportive-02);
+`;
+
+const CSSImportant = css`
+  ${CSSSolidLabelBase}
+  background: var(--ui-supportive-01);
+  color: var(--text-02);
+`;
+
+const CSSGreen = css`
+  ${CSSSolidLabelBase}
+  background: var(--ui-supportive-04);
+  color: var(--text-07);
+`;
+
+const CSSRed = css`
+  ${CSSSolidLabelBase}
+  background: var(--ui-supportive-02);
+  color: var(--text-06);
+`;
+
+const CSSYellow = css`
+  ${CSSSolidLabelBase}
+  background: var(--ui-supportive-03);
+  color: var(--text-08);
+`;
+
+const CSSGrey = css`
+  ${CSSSolidLabelBase}
+  background: var(--ui-03);
+  color: var(--text-03);
+`;
+
+const CSSGreyOutline = css`
+  ${CSSSolidLabelBase}
+  box-sizing: border-box;
+  background: var(--ui-01);
+  border: 1px solid var(--ui-03);
+  color: var(--text-03);
+`;
+
+const getTypeStyling = (type: ISolidLabelType) => {
+  switch (type) {
+    case "important":
+      return CSSImportant;
+    case "green":
+      return CSSGreen;
+    case "red":
+      return CSSRed;
+    case "yellow":
+      return CSSYellow;
+    case "grey":
+      return CSSGrey;
+    case "greyOutline":
+      return CSSGreyOutline;
+    default:
+      return CSSDefault;
+  }
+};
+
+const Bronze = css<ISolidLabelProps>`
+  ${(props) => getTypeStyling(props.type)}
+  cursor: pointer;
+`;
+
+const Silver = css``;
+
+const Gold = css``;
+
+const Platinum = css``;
+
+const Diamond = css``;
+
+export const SSolidLabel = styled.button<any>`
+  ${Bronze}
+  ${MIN_SILVER`${Silver}`};
+  ${MIN_GOLD`${Gold}`};
+  ${MIN_PLATINUM`${Platinum}`};
+  ${MIN_DIAMOND`${Diamond}`};
+`;
