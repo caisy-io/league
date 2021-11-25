a5faf70911f132ec7d82f343a99f6e60d2992d01
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-11-25 10:57:31 +0000

    finish text and image preview, WIP icon preview

---
+++ src/components/preview-icon/PreviewIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SPreviewIcon } from "./styles/SPreviewIcon";
+export interface IPreviewIcon {
+  icon?: React.ReactNode;
+  size: number;
+}
+
+export const PreviewIcon: React.FC<IPreviewIcon> = ({ icon, ...props }) => {
+  return (
+    <SPreviewIcon size={props.size}>
+      {icon}
+    </SPreviewIcon>
+  );
+};
