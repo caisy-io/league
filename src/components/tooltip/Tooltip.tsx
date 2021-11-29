511fea94c32d6577f81bd09a43dc8155b37821ce
Nicolas Shiken <nicolasshiken@gmail.com>
2021-11-29 16:35:34 -0300

    Update Tooltip.tsx

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, FC, useState, useRef } from "react";
 import { STooltip } from "./styles/STooltip";
 import { STooltipWrapper } from "./styles/STooltipWrapper";
 import { Popover, TPlacement } from "../popover/Popover";
@@ -7,15 +7,14 @@ type TTooltipColor = "white" | "black";
 
 export interface ITooltip {
   content: ReactNode | (() => ReactNode);
-  supressArrow?: boolean | undefined;
   placement?: TPlacement | undefined;
   color?: TTooltipColor;
 }
 
-export const Tooltip: React.FC<ITooltip> = ({ ...props }) => {
+export const Tooltip: FC<ITooltip> = ({ content, placement, color, children }) => {
   // console.log(`Tooltip props: `, props);
-  const [open, setOpen] = React.useState(false);
-  const [closing, setClosing] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [closing, setClosing] = useState(false);
   let closeTooltipTimeout;
   let tooltipDelayTimeout;
   const closeTooltip = () => {
@@ -43,10 +42,10 @@ export const Tooltip: React.FC<ITooltip> = ({ ...props }) => {
     clearTimeout(closeTooltipTimeout);
   };
 
-  const ref = React.useRef(null);
+  const ref = useRef(null);
 
   const getBackgroundColor = () => {
-    switch (props.color) {
+    switch (color) {
       case "black":
         return "var(--ui-overlay-02)";
       case "white":
@@ -56,7 +55,7 @@ export const Tooltip: React.FC<ITooltip> = ({ ...props }) => {
   };
 
   const getColor = () => {
-    switch (props.color) {
+    switch (color) {
       case "black":
         return "var(--text-02)";
       case "white":
@@ -67,11 +66,11 @@ export const Tooltip: React.FC<ITooltip> = ({ ...props }) => {
 
   return (
     <STooltipWrapper ref={ref} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {props.children}
+      {children}
       {open && (
-        <Popover trianglecolor={getBackgroundColor()} placement={props.placement || "top"} reference={ref}>
+        <Popover trianglecolor={getBackgroundColor()} placement={placement || "top"} reference={ref}>
           <STooltip closing={closing} color={getColor()} backgroundColor={getBackgroundColor()}>
-            {props.content}
+            {content}
           </STooltip>
         </Popover>
       )}
