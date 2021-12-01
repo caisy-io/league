0fa9029aff399b0ac675247e97c4666da3feaa3a
Cristian Martinez <cristian.pablo3403@gmail.com>
2021-12-01 17:49:23 -0300

    Added files

---
+++ src/icons/styles/SIconBase.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const SIconBase = styled.svg`
+  path {
+    stroke: var(--icon-01);
+  }
+
+  path.overwrite {
+    stroke: var(--icon-05);
+    fill: var(--icon-05);
+  }
+`
---
+++ src/utils/types/IIconSize.tsx
@@ -0,0 +1,3 @@
+export interface IIconSize {
+  size: 16 | 20 | 24 | 32;
+}
