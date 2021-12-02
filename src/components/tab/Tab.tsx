cb0101361a575fcdea082005a80a4edd0e7b2954
Nicolas Shiken <nicolasshiken@gmail.com>
2021-12-01 22:45:38 -0300

    Update Tab.tsx

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ISizesWithDefault } from "../..";
 import { STab } from "./styles/STab";
 
@@ -6,13 +6,19 @@ interface ITab {
   size?: ISizesWithDefault;
   activated?: boolean;
   icon?: JSX.Element;
+  key: string;
+  onClick?: (value: string) => void;
 }
 
-export const Tab: React.FC<ITab> = ({ size, activated, icon, ...props }) => {
+export const Tab: React.FC<ITab> = ({ size, activated, icon, onClick, key, ...props }) => {
   const onlyIcon = !!icon && !props.children;
 
+  const handleClick = () => {
+    onClick?.(key);
+  };
+
   return (
-    <STab activated={activated} onlyIcon={onlyIcon} size={size}>
+    <STab activated={activated} onlyIcon={onlyIcon} size={size} onClick={handleClick}>
       {icon && icon}
       {props.children}
     </STab>
