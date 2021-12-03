f78cf0faed2adf1259f858f6aec16d25e0fa18db
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-12-03 21:57:24 +0100

    text-sidebar-title-cy-405 (#70)
    * wip: init sidebar title
    
    * wip: insert switch
    
    * wip: add wrapper element
    
    * cleanup code and finish component
    
    * update styling with margin
    
    * update styling with margin
    
    * fix svg shrink bug
    
    * center text (finally)
    
    * enable smaller version of toggle to fit component
    
    * move toggle from props to component
    
    * update toggle component
    
    * update story
    
    * handle long input and cleanup props
    
    * fix icon size
    
    * fix behaviour with long titles
    
    * fix width issues

--- src/components/toggle/styles/SToggle.tsx
+++ src/components/toggle/styles/SToggle.tsx
@@ -1,13 +1,22 @@
 import styled, { css } from "styled-components";
 import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles/mediaquerys";
+import { IToggleProps } from "../Toggle";
 
-const Bronze = css`
+const CSSSmall = css`
+  width: 1.625rem;
+  height: 1rem;
+  input:checked + span:before {
+    transform: translateX(0.5rem) translateY(-50%);
+  }
+`;
+
+const Bronze = css<IToggleProps>`
   position: relative;
-  width: 38px;
-  height: 20px;
+  width: 2.375rem;
+  height: 1.25rem;
   display: inline-block;
   input:checked + span:before {
-    transform: translateX(16px) translateY(-50%);
+    transform: translateX(1rem) translateY(-50%);
   }
   input + span {
     transition: all 0.5s ease;
@@ -16,6 +25,7 @@ const Bronze = css`
   input:checked + span {
     background-color: var(--active-interactional-tertiary-02);
   } 
+  ${(props) => props.small  ? CSSSmall : ''};
 `;
 
 const Silver = css``;
@@ -26,7 +36,7 @@ const Platinum = css``;
 
 const Diamond = css``;
 
-export const SToggle = styled.label`
+export const SToggle = styled.label<IToggleProps>`
   ${Bronze}
   ${MIN_SILVER`${Silver}`};
   ${MIN_GOLD`${Gold}`};
