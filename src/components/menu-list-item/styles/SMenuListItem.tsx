85e2d3918d20ea7cc6d6c29613fe9bda6c203fd8
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-12-06 14:23:19 +0000

    fix disabled status

--- src/components/menu-list-item/styles/SMenuListItem.tsx
+++ src/components/menu-list-item/styles/SMenuListItem.tsx
@@ -1,20 +1,23 @@
 import styled, { css } from 'styled-components';
-import { CSSProgressiveBody03Medium, CSSProgressiveCaption01Medium } from '../../../constants/styles/design-tokens/fonts/CSSTypographies';
+import { CSSProgressiveBody02Medium, CSSProgressiveCaption01Medium } from '../../../constants/styles/design-tokens/fonts/CSSTypographies';
+import { CSSProgressiveBody03Medium } from '../../../constants/styles/fonts';
 import { MIN_DIAMOND, MIN_GOLD, MIN_PLATINUM, MIN_SILVER } from '../../../constants/styles/mediaquerys';
 import { IMenuListItemProps } from '../MenuListItem';
 
 const CSSSmall = css`
-  height:44px;
+  height: 44px;
   ${CSSProgressiveBody03Medium};
+  line-height: 14px;
 `;
 
 const CSSMedium = css`
-  height:48px;
+  height: 48px;
 `;
 
 const CSSDisabled = css`
   background-color: var(--ui-02);
   color: var(--disabled-text);
+  cursor: auto;
   div, div:nth-of-type(2) {
     color: var(--disabled-text);
     svg{
@@ -29,7 +32,8 @@ const Bronze = css<IMenuListItemProps>`
   align-items: center;
   box-sizing: border-box;
   width: 100%;
-  ${CSSProgressiveBody03Medium};
+  ${CSSProgressiveBody02Medium};
+  line-height: 16px;
   color: var(--text-01);
   background-color: transparent;
   border-bottom: 1px solid var(--ui-03);
@@ -56,6 +60,7 @@ const Bronze = css<IMenuListItemProps>`
   }
   div:nth-of-type(2){
     ${CSSProgressiveCaption01Medium};
+    line-height: 12px;
     color: var(--text-04);
     svg{
       color: var(--text-01);
@@ -65,9 +70,12 @@ const Bronze = css<IMenuListItemProps>`
   ${(props) => props.size == "medium" ? CSSMedium : ''};
   &:hover {
     background-color: var(--hover-ui-01);
+    cursor: pointer;
+    ${(props) => props.disabled ? CSSDisabled : ''};
   }
   &:active {
     background-color: var(--active-ui-01-1);
+    ${(props) => props.disabled ? CSSDisabled : ''};
   }
   ${(props) => props.disabled ? CSSDisabled : ''};
 `;
