f4137216ac0af8d65454d479e636141750a2f3ca
Cristian <cristian_22380@hotmail.com>
2021-12-07 08:57:13 -0300

    main-navigation-icons-cy-95 (#91)
    * Main Nvigation Icons
    
    * Added files
    
    * Fixed for build
    
    * Fixed
    
    * Fixed comments
    
    * Fixed
    
    * Fixed
    
    Co-authored-by: Cristian Martinez <cristian.pablo3403@gmail.com>

---
+++ src/icons/Icons.stories.tsx
@@ -0,0 +1,65 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React, { FC } from "react";
+import { 
+  IconDocumentsComponents,
+  IconDocuments, 
+  IconAssetsImage,
+  IconCalendar,
+  IconSchemas,
+  IconTerminal,
+  IconNotificationBell,
+  IconSearch,
+  IconChat,
+  IconUpload,
+  IconComponents
+} from './index';
+
+const icons = {
+  IconDocumentsComponents: ({size}) => <IconDocumentsComponents size={size}/>,
+  IconDocuments: ({size}) => <IconDocuments size={size}/>,
+  IconAssetsImage: ({size}) => <IconAssetsImage size={size}/>,
+  IconCalendar: ({size}) => <IconCalendar size={size}/>,
+  IconSchemas: ({size}) => <IconSchemas size={size}/>,
+  IconTerminal: ({size}) => <IconTerminal size={size}/>,
+  IconNotificationBell: ({size}) => <IconNotificationBell size={size}/>,
+  IconSearch: ({size}) => <IconSearch size={size}/>,
+  IconChat: ({size}) => <IconChat size={size}/>,
+  IconUpload: ({size}) => <IconUpload size={size}/>,
+  IconComponents: ({size}) => <IconComponents size={size}/>,
+};
+
+interface IMainMenuIconDemo {
+  size?: 16 | 20 | 24 | 32;
+  icon?: string;
+}
+
+const MainMenuIconDemo: FC<IMainMenuIconDemo> = ({ ...args }) => {
+  const Icon = icons[args.icon]
+  return <Icon size={args.size}/>;
+};
+
+export default {
+  title: "Icons/MainMenu/Icon",
+  component: MainMenuIconDemo,
+  argTypes: {
+    size: {
+      name: "size",
+      description: "Size of the icon",
+      options: [16, 20, 24, 32],
+      control: { type: 'select' }
+    },
+    icon: {
+      name: "icon",
+      description: "Icon name",
+      options: ['IconDocumentsComponents', 'IconAssetsImage', 'IconCalendar', 'IconSchemas', 'IconTerminal', 'IconNotificationBell', 'IconSearch', 'IconChat', 'IconUpload', 'IconDocuments', 'IconComponents'],
+      control: { type: 'select' }
+    }
+  },
+} as ComponentMeta<typeof MainMenuIconDemo>;
+
+const Template:  ComponentStory<typeof MainMenuIconDemo>= (args) => <MainMenuIconDemo  {...args} />;
+
+export const MainMenuIcons = Template.bind({});
+MainMenuIcons.args = {
+  icon: 'IconDocuments',
+};
--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,1 +1,2 @@
 export * from './types';
+export { IIconSize } from './IIconSize';
