0e066dd10d9bd1abdaa3992ca015904cbc4c64ee
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-12-08 09:53:19 +0000

    recover deleted files

---
+++ src/components/organization-menu-list-item/styles/SOrganizationMenuListItemTextWrapper.tsx
@@ -0,0 +1,23 @@
+import styled, { css } from 'styled-components';
+import { MIN_DIAMOND, MIN_GOLD, MIN_PLATINUM, MIN_SILVER } from '../../../constants/styles/mediaquerys';
+
+const Bronze = css`
+  position: relative;
+  width: 75%;
+`;
+
+const Silver = css``;
+
+const Gold = css``;
+
+const Platinum = css``;
+
+const Diamond = css``;
+
+export const SOrganizationMenuListItemTextWrapper = styled.div`
+  ${Bronze}
+  ${MIN_SILVER`${Silver}`};
+  ${MIN_GOLD`${Gold}`}; 
+  ${MIN_PLATINUM`${Platinum}`};
+  ${MIN_DIAMOND`${Diamond}`};
+`;
