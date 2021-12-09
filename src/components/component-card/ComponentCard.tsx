b88f73f24b38785df680d9586586b2995b20f753
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-12-09 16:38:45 +0000

    setup component

--- src/components/component-card/ComponentCard.tsx
+++ src/components/component-card/ComponentCard.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { SFlex } from '../../base-components/flex/styles/SFlex';
+import { IconTags } from '../../icons';
 import { Card } from '../card';
 import { SComponentCard } from './styles/SComponentCard';
+import { SComponentCardDescription } from './styles/SComponentCardDescription';
+import { SComponentCardTitle } from './styles/SComponentCardTitle';
+import { STagText } from './styles/STagText';
 
 interface IComponentCard {
-
+  title?: string | undefined;
+  description?: string | undefined;
 }
 
-export const ComponentCard: React.FC<IComponentCard> = ({ ...props }) => {
-  
+export const ComponentCard: React.FC<IComponentCard> = ({title, description, children}) => {
+
   return (
     <SComponentCard>
       <Card>
-<IconTag
+        <SComponentCardTitle>{title}</SComponentCardTitle>
+        <SComponentCardDescription>{description}</SComponentCardDescription>
+        <SFlex>
+          <IconTags></IconTags>
+          <STagText>Tag A, Tag B, Tag C</STagText>
+        </SFlex>
+
       </Card>
-      {props.children}
+      {children}
     </SComponentCard>
   )
 }
