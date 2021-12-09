ee98fafddab036bdd50040f7fcc7bcc9054619f6
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-12-09 08:33:37 +0000

    correct forward ref

--- src/components/scrollbar/Scrollbar.tsx
+++ src/components/scrollbar/Scrollbar.tsx
@@ -1,12 +1,12 @@
 import React, { FC, forwardRef } from 'react';
 import { SScrollbar } from './styles/SScrollbar';
 
-interface IScrollbarProps{
+interface IScrollbarProps {
   children?,
   ref?
 }
 
-export const Scrollbar: FC<IScrollbarProps> = forwardRef((children, ref) => {
+export const Scrollbar: FC<IScrollbarProps> = forwardRef(({ children }: IScrollbarProps, ref: any) => {
   return (
     <SScrollbar ref={ref}>
       {children}
