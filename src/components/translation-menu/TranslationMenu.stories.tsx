c3878c60729446834192e8748db3dfb85bfe4ed6
Nicolas Shiken <nicolasshiken@gmail.com>
2021-12-13 14:24:17 -0300

    Translation Menu now uses the correct List Item

--- src/components/translation-menu/TranslationMenu.stories.tsx
+++ src/components/translation-menu/TranslationMenu.stories.tsx
@@ -2,21 +2,23 @@ import React, { useState } from "react";
 import { TranslationMenu } from "../..";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { ITranslationMenuLocale } from ".";
+import { ILanguageFlagToggleListItemLocale } from "..";
 
 function TranslationMenuDemo() {
-  const [opened, setOpened] = useState<boolean>(false);
-  const [locales, setLocales] = useState<ITranslationMenuLocale[]>([
-    { id: "en", name: "English", default: true, active: true },
+  const [opened, setOpened] = useState<boolean>(true);
+  const [locales, setLocales] = useState<ILanguageFlagToggleListItemLocale[]>([
+    { id: "en" as any, name: "English", active: true },
     { id: "de", name: "Deutsch", active: true },
     { id: "es", name: "Spanish", active: true },
     { id: "fr", name: "French", active: false },
-    { id: "ru", name: "Russisch", active: true },
+    { id: "pt", name: "Portuguese", active: true },
   ]);
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <TranslationMenu locales={locales} onLocalesChange={setLocales} opened={opened} onToggleOpened={setOpened} />
+      <div style={{ width: 300 }}>
+        <TranslationMenu locales={locales} onLocalesChange={setLocales} opened={opened} onToggleOpened={setOpened} />
+      </div>
       <div
         style={{
           width: 300,
