82b892b2e91dca8476159ce697b8c37f9f79c7a6
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-12-14 15:50:26 +0000

    fix svg properties

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -20,7 +20,7 @@ export const Checkbox: React.FC<ICheckbox> = ({ loading, onChange }) => {
 
 
         <svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.62793 3.22613L2.0732 4.6714L2.06386 4.66206L5.32455 1.40137" stroke="white" stroke-width="1.2" stroke-dasharray="200" stroke-dashoffset="200" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M0.62793 3.22613L2.0732 4.6714L2.06386 4.66206L5.32455 1.40137" stroke="white" strokeWidth="1.2" strokeDasharray="200" strokeDashoffset="200" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
 
       </SCustomCheckbox>
