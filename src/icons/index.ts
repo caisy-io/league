14b14f29fdd9689fdb87c120a26468ae67e222c5
Cristian Martinez <cristian.pablo3403@gmail.com>
2021-12-16 11:26:51 -0300

    Form Icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -128,7 +128,56 @@ export { IconChevron } from "./IconChevron";
 export { IconUpload } from "./IconUpload";
 export { IconDocuments } from "./IconDocuments";
 export { IconComponents } from "./IconComponents";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export { IconChevronRight } from  "./IconChevronRight";
 export { IconChevronLeft } from  "./IconChevronLeft";
 export { IconChevronUp } from  "./IconChevronUp";
 export { IconCross } from "./IconCross";
+export { IconCrossCircle } from "./IconCrossCircle";
+export { IconCircle } from "./IconCircle";
+export { IconCheckmarkCircle } from "./IconCheckmarkCircle";
