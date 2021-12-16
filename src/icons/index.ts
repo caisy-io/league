e5b6df712fb7688e93a4096d559731fda3920fdc
Cristian Martinez <cristian.pablo3403@gmail.com>
2021-12-16 14:58:38 -0300

    Action Icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -128,7 +128,60 @@ export { IconChevron } from "./IconChevron";
 export { IconUpload } from "./IconUpload";
 export { IconDocuments } from "./IconDocuments";
 export { IconComponents } from "./IconComponents";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export { IconChevronRight } from  "./IconChevronRight";
 export { IconChevronLeft } from  "./IconChevronLeft";
 export { IconChevronUp } from  "./IconChevronUp";
 export { IconCross } from "./IconCross";
+
+
+
+export { IconDuplicate } from "./IconDuplicate";
+export { IconArchive } from "./IconArchive";
+export { IconDelete } from "./IconDelete";
+export { IconBookmark } from "./IconBookmark";
