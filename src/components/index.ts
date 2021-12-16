b6983858531993e33eab6e8287575cb0ad81a530
Nicolas Shiken <nicolasshiken@gmail.com>
2021-12-16 10:03:10 -0300

    added index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -44,6 +44,7 @@ export * from "./stackable";
 export * from "./status";
 export * from "./toggle";
 export * from "./table";
+export * from "./table-row";
 export * from "./tab";
 export * from "./tabs";
 export * from "./theme";
