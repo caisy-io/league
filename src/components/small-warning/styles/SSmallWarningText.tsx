19572e1b5fbd41d8e474930b64012bc5b90fb0e8
steiningermona <93647258+steiningermona@users.noreply.github.com>
2021-12-17 12:20:50 +0100

    labels-small-warning-cy-524 (#122)
    * setup small warning component and add warning hex icon
    
    * update story and add overwrite pattern
    
    * add size property to icon and component
    
    Co-authored-by: Mona Steininger <monasteininger@Monas-MacBook-Pro.local>

---
+++ src/components/small-warning/styles/SSmallWarningText.tsx
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components';
+import { CSSProgressiveCaption01Medium } from '../../../constants/styles/design-tokens/fonts/CSSTypographies';
+import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from '../../../constants/styles/mediaquerys';
+
+const Bronze = css`
+  ${CSSProgressiveCaption01Medium};
+  line-height: 0.75rem;
+`;
+
+const Silver = css``;
+
+const Gold = css``;
+
+const Platinum = css``;
+
+const Diamond = css``;
+
+export const SSmallWarningText = styled.p`
+  ${Bronze}
+  ${MIN_SILVER`${Silver}`};
+  ${MIN_GOLD`${Gold}`}; 
+  ${MIN_PLATINUM`${Platinum}`};
+  ${MIN_DIAMOND`${Diamond}`};
+`;
+
