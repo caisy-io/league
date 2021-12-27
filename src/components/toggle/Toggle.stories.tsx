5fa19e97982a59bb6320209203d81b420bd31414
Nicolas Shiken <nicolasshiken@gmail.com>
2021-12-27 14:56:27 -0300

    added `size` argtype

--- src/components/toggle/Toggle.stories.tsx
+++ src/components/toggle/Toggle.stories.tsx
@@ -4,17 +4,28 @@ import { Toggle } from "./Toggle";
 export default {
   title: `Components/Forms/Toggle`,
   component: ToggleDemo,
-  argTypes: {},
+  argTypes: {
+    size: {
+      description: "Changes the size of the Toggle",
+      control: { type: "select" },
+      options: ["small", "default"],
+      table: {
+        defaultValue: {
+          summary: "default",
+        },
+      },
+    },
+  },
 };
 
-function ToggleDemo({ small }) {
+function ToggleDemo({ size }) {
   const [activated, setActivated] = useState(false);
-  return <Toggle size={small ? "small" : undefined} activated={activated} onChange={setActivated} />;
+  return <Toggle size={size} activated={activated} onChange={setActivated} />;
 }
 
 const Template = (args) => <ToggleDemo {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  small: false,
+  size: "default",
 };
