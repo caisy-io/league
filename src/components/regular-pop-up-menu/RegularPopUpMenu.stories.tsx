b8c949f2ebf14392ee734d7d93d6ea4467734925
Cristian Martinez <cristian.pablo3403@gmail.com>
2021-12-28 14:18:49 -0300

    Fixed

--- src/components/regular-pop-up-menu/RegularPopUpMenu.stories.tsx
+++ src/components/regular-pop-up-menu/RegularPopUpMenu.stories.tsx
@@ -3,15 +3,7 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React, { FC, useState } from "react";
 import RegularPopUpMenu from '.';
 import { IRegularPopUpMenu } from "./RegularPopUpMenu";
-import { ModalHeader } from "..";
-import { ModalHeaderTitle } from "..";
-import { SimpleInput } from "../input-fields/simple-input/SimpleInput";
-import { MenuListItem } from "../menu-list-item/MenuListItem";
 import { IconStarOutlined } from "../..";
-import { SPopUpHeader } from "./styles/SPopUpHeader";
-import { SPopUpInput } from "./styles/SPopUpInput";
-import { SPopUpItem } from "./styles/SPopUpItem";
-import { SPopUpContent } from './styles/SPopUpContent';
 
 const STooglePopUp = styled.h2`
   cursor: pointer;
@@ -27,54 +19,47 @@ interface IRegularPopUpMenuMemo extends IRegularPopUpMenu {
 const RegularPopUpMenuDemo: FC<IRegularPopUpMenuMemo> = ({listItems}) => {
   const [value, setValue] = useState("");
   const [toogleModal, setToogleModal] = useState(false);
+
+  const onToggleModal: any = () => {
+    setToogleModal(!toogleModal);
+  } 
   const onChange = (e) => {
     setValue(e.target.value);
   };
   return (
     <>
     {toogleModal ? (
-        <RegularPopUpMenu>
-          <SPopUpHeader>
-            <ModalHeader onClose={() => setToogleModal(false)}>
-              <ModalHeaderTitle>Label</ModalHeaderTitle>
-            </ModalHeader>
-          </SPopUpHeader>
-
-          <SPopUpContent>
-            <SPopUpInput>
-              <SimpleInput label='Label' placeholder="Default" onChange={onChange} value={value}/>
-            </SPopUpInput>
-
-            {listItems.map((item, ind) =>
-              <SPopUpItem key={`${ind}-menu-item`}>
-                <MenuListItem >
-                  <div> <IconStarOutlined />  <span>{`${item} ${ind + 1}`}</span>  </div>
-                </MenuListItem>
-              </SPopUpItem>
-            )}
-          </SPopUpContent> 
-        </RegularPopUpMenu>
+        <RegularPopUpMenu 
+          onClose={onToggleModal}
+          headerLabel="Label"
+          onChange={onChange}
+          inputValue={value}
+          menuItemsList={listItems}
+          inputLabel="Label"
+          placeholder="Defalut"
+          icon={<IconStarOutlined />}
+        />
       ) : (
-        <STooglePopUp onClick={() => setToogleModal(true)}>Click me!!</STooglePopUp>
+        <STooglePopUp onClick={() => onToggleModal()}>Click me!!</STooglePopUp>
     )}
     </>
   );
 };
 
 export default {
-  title: "Components/RegularPopUpmenu/PopUp",
+  title: "Components/Menus & Popovers/RegularPopUpmenu",
   component: RegularPopUpMenuDemo,
 
 } as ComponentMeta<typeof RegularPopUpMenuDemo>;
 
 const Template:  ComponentStory<typeof RegularPopUpMenuDemo>= (args) => <RegularPopUpMenuDemo  {...args} />;
 
-export const Regular_Pop_Up_Menu_with_Scroll = Template.bind({});
-Regular_Pop_Up_Menu_with_Scroll.args = {
-  listItems: ["Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default"],
+export const Simple = Template.bind({});
+Simple.args = {
+  listItems: ["Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default"],
 };
 
-export const Regular_Pop_Up_Menu = Template.bind({});
-Regular_Pop_Up_Menu.args = {
-  listItems: ["Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default"],
+export const With_Scrollbar = Template.bind({});
+With_Scrollbar.args = {
+  listItems: ["Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default", "Default"],
 };
--- src/components/regular-pop-up-menu/RegularPopUpMenu.tsx
+++ src/components/regular-pop-up-menu/RegularPopUpMenu.tsx
@@ -1,14 +1,47 @@
 import React, { FC, ReactNode } from 'react';
+import { ModalHeader } from "..";
+import { ModalHeaderTitle } from "..";
+import { MenuListItem } from "../menu-list-item/MenuListItem";
+import { SimpleInput } from "../input-fields/simple-input/SimpleInput";
 import { SRegularPopUpMenu } from "./styles/SRegularPopUpMenu";
+import { SPopUpHeader } from "./styles/SPopUpHeader";
+import { SPopUpContent } from "./styles/SPopUpContent";
+import { SPopUpInput } from "./styles/SPopUpInput";
+import { SPopUpItem } from "./styles/SPopUpItem";
 
 export interface IRegularPopUpMenu {
-  children: ReactNode
+  onClose?: () => any;
+  headerLabel?: string;
+  menuItemsList?: Array<ReactNode>;
+  onChange?: any;
+  inputValue?: any;
+  inputLabel?: string;
+  placeholder?: string;
+  icon?: ReactNode; 
 }
 
-const RegularPopUpMenu: FC<IRegularPopUpMenu> = (props) => {
+const RegularPopUpMenu: FC<IRegularPopUpMenu> = ({onClose, headerLabel, onChange, inputValue, menuItemsList, inputLabel, placeholder, icon }) => {
   return (
     <SRegularPopUpMenu>
-      {props.children}
+      <SPopUpHeader>
+        <ModalHeader onClose={onClose}>
+          <ModalHeaderTitle>{headerLabel}</ModalHeaderTitle>
+        </ModalHeader>
+      </SPopUpHeader>
+
+      <SPopUpContent>
+        <SPopUpInput>
+          <SimpleInput label={inputLabel} placeholder={placeholder} onChange={onChange} value={inputValue}/>
+        </SPopUpInput>
+
+        {menuItemsList?.map((item, ind) =>
+          <SPopUpItem key={`${ind}-menu-item`}>
+            <MenuListItem >
+              <div> {icon && icon} <span>{`${item} ${ind + 1}`}</span> </div>
+            </MenuListItem>
+          </SPopUpItem>
+        )}
+      </SPopUpContent> 
     </SRegularPopUpMenu>
   )
 }
