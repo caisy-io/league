237115fe5dd7694ef51614f6488d5d4d8d988a1a
bredek <eugene.bredyuk@gmail.com>
2021-12-29 17:47:03 +0200

    very dirty and rough component showcase

--- src/components/set-image-card/SetImageCard.tsx
+++ src/components/set-image-card/SetImageCard.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
-import {SSetImageCard } from './styles/SSetImageCard';
+import React, { useState } from "react";
+import { Button } from "../button";
+import {
+  SSetImageCard,
+  SSetImageBody,
+  SSetImageHeader,
+  SSetImageTitle,
+  SSetImageSubtitle,
+} from "./styles/SSetImageCard";
 
-interface ISetImageCard{
-  onChange: (url: string) => void,
-  processImage: (file: File) => Promise<string>
-  initalValue?: string,
+interface ISetImageCard {
+  onChange: (url: string) => void;
+  processImage: (file: File) => Promise<string>;
+  initalValue?: string;
 }
 
-export const SetImageCard: React.FC<ISetImageCard> = ({children}) => {
+export const SetImageCard: React.FC<ISetImageCard> = () => {
+  const [image, setImage] = useState<string | null>(null);
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  const mockProcessIamge = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setImage("https://picsum.photos/200");
+      setLoading(false);
+    }, 2000);
+  };
+
   return (
     <SSetImageCard>
-      {children}
+      <SSetImageBody>
+        <SSetImageHeader>
+          {isLoading && <SSetImageTitle>Loading...</SSetImageTitle>}
+          {!isLoading && !image && (
+            <>
+              <SSetImageTitle>Drag and drop file to upload</SSetImageTitle>
+              <SSetImageSubtitle>Supported formats: .jpg, .png, .gif, .webp or video file</SSetImageSubtitle>
+            </>
+          )}
+        </SSetImageHeader>
+        {image && <img src={image} />}
+        {!isLoading && !image && (
+          <Button type="primary" onClick={mockProcessIamge}>
+            SELECT FILE
+          </Button>
+        )}
+      </SSetImageBody>
     </SSetImageCard>
-  )
-}
+  );
+};
--- src/components/set-image-card/styles/SSetImageCard.tsx
+++ src/components/set-image-card/styles/SSetImageCard.tsx
@@ -1,7 +1,17 @@
-import styled, { css } from 'styled-components';
-import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from '../../../constants/styles/mediaquerys';
+import styled, { css } from "styled-components";
+import { CSSProgressiveHeading01, CSSProgressiveBody02Medium } from "../../../constants/styles/fonts";
+import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles/mediaquerys";
 
-const Bronze = css``;
+const Bronze = css`
+  width: 460px;
+  height: 520px;
+  background-color: var(--ui-background);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const Silver = css``;
 
@@ -11,11 +21,39 @@ const Platinum = css``;
 
 const Diamond = css``;
 
+export const SSetImageBody = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  flex-direction: column;
+`;
+
+export const SSetImageHeader = styled.div`
+  text-align: center;
+`;
+
+export const SSetImageTitle = styled.p`
+  ${CSSProgressiveHeading01};
+  color: var(--text-01);
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const SSetImageSubtitle = styled.p`
+  margin-bottom: 32px;
+
+  ${CSSProgressiveBody02Medium};
+  color: var(--text-01);
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
 export const SSetImageCard = styled.div`
   ${Bronze}
   ${MIN_SILVER`${Silver}`};
-  ${MIN_GOLD`${Gold}`}; 
+  ${MIN_GOLD`${Gold}`};
   ${MIN_PLATINUM`${Platinum}`};
   ${MIN_DIAMOND`${Diamond}`};
 `;
-
