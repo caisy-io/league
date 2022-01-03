102dfbe65624d069e6cf5bfd27e39eb19c472610
Nicolas Shiken <nicolasshiken@gmail.com>
2022-01-03 12:21:25 -0300

    changed the onClick typing

--- src/components/icon-button/IconButton.tsx
+++ src/components/icon-button/IconButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 import { ISizesSmallMedium } from "../../interfaces";
 import { SIconButton } from "./styles/SIconButton";
 
@@ -6,12 +6,12 @@ interface IIconButton {
   size?: ISizesSmallMedium;
   type?: "primary" | "secondary" | "danger";
   activated?: boolean;
-  onClick?: (newActivatedValue: boolean) => void;
+  onClick?: (e: MouseEventHandler<HTMLDivElement>) => void;
 }
 
 export const IconButton: React.FC<IIconButton> = ({ children, size, type, activated, onClick }) => {
   return (
-    <SIconButton onClick={() => onClick?.(!activated)} activated={activated} size={size} type={type}>
+    <SIconButton onClick={onClick} activated={activated} size={size} type={type}>
       {children}
     </SIconButton>
   );
