b76c527583881e2334dc92162fcd7e9b6706638e
steiningermona <93647258+steiningermona@users.noreply.github.com>
2022-01-03 14:32:00 +0100

    select-input-simple-cy-159 (#166)
    * component structure and title+label
    
    * styling and handling text input
    
    * get single select working and add label
    
    * single select works WIP styling
    
    * update styling WIP required
    
    * clear duplicate naming of onchange
    
    * dropdown working WIP language flag
    
    * enable language flag and arrow in right position
    
    * update styling
    
    * add to do to singleselect.tsx file
    
    * cleanup code and delete unused files

---
+++ src/components/input-dropdown/Select.stories.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { IconStarOutlined } from "../../icons/IconStarOutlined";
+import { SimpleInputDropdown } from "./SimpleInputDropdown";
+
+export default {
+  title: "Components/Forms/SimpleInputDropdown",
+  component: SimpleInputDropdownDemo,
+  argTypes: {
+    required: {
+      description: "An example boolean argument type",
+      control: { type: "boolean" },
+    },
+    error: {
+      description: "An example boolean argument type",
+      control: { type: "boolean" },
+    },
+    translationBadge: {
+      description: "An example boolean argument type",
+      control: { type: "boolean" },
+    },
+  },
+};
+
+const dataSource = [
+  {
+    key: "title_label",
+    title: "Default",
+    label: "Label"
+  },
+  {
+    key: "icon_title",
+    title: "Title and Icon",
+    icon: <IconStarOutlined size={20}></IconStarOutlined>,
+  },
+  {
+    key: "icon_title_label",
+    title: "Label, Title and Icon",
+    label: "Label",
+    icon: <IconStarOutlined size={20}></IconStarOutlined>,
+  },
+];
+
+function SimpleInputDropdownDemo({ error, required, translationBadge }) {
+  const [selectValue, setSelectValue] = React.useState(null);
+  return <SimpleInputDropdown
+    error={error}
+    required={required}
+    translationBadge={translationBadge}
+    dataSource={dataSource}
+    placeholder="Select an option.."
+    value={selectValue}
+    onSelectValue={(e) => setSelectValue(e)}
+  />
+}
+
+const Template = (args) => <SimpleInputDropdownDemo {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  error: false,
+  required: false,
+  translationBadge: false,
+};
+
+export const Required = Template.bind({});
+Required.args = {
+  error: false,
+  required: true,
+  translationBadge: false,
+};
+
+export const Translation = Template.bind({});
+Translation.args = {
+  error: false,
+  required: false,
+  translationBadge: true,
+};
---
+++ src/components/input-dropdown/TranslationBadge.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Flag from '../flag';
+import { STranslationBadge } from './styles/STranslationBadge';
+
+export interface ITranslationBadgeProps {
+  countryCode: any;
+}
+
+export const TranslationBadge: React.FC<ITranslationBadgeProps> = ({countryCode }) => {
+  return (
+    <STranslationBadge>
+      <Flag countryCode={countryCode} size={16} />
+      DE
+    </STranslationBadge>
+  )
+};
