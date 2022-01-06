458330a77cc7fd7f44148abfe8228c6bc31b8d28
Cristian Martinez <cristian.pablo3403@gmail.com>
2022-01-06 11:45:05 -0300

    Fixed

--- src/components/buttons-bar/styles/SButtonsBar.tsx
+++ src/components/buttons-bar/styles/SButtonsBar.tsx
@@ -9,6 +9,10 @@ const Bronze = css<IButtonsBar>`
   flex: 1;
   box-shadow: 0px -4px 8px rgba(0, 0, 0, 0.04);
 
+  button:not(:last-child) {
+    border-right: 1px solid var(--ui-03);
+  }
+
   > button {
     flex: 1;
     height: 44px;
