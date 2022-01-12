216a5f81aadfe08ffeeda3a81338f78f41c6ca1c
Cristian Martinez <cristian.pablo3403@gmail.com>
2022-01-12 10:33:36 -0300

    Fix

--- src/components/uploads-pop-up/UloadsPopupButtons.tsx
+++ src/components/uploads-pop-up/UloadsPopupButtons.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { SUloadsPopupButtonsWrapper } from "./styles/SUloadsPopupButtonsWrapper"
+
 export const UloadsPopupButtons: FC = ({ children }) => (
   <SUloadsPopupButtonsWrapper>
     {children}
