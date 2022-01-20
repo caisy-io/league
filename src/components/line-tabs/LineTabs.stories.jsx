0a9fc6a697169bb160194865f2e85c171eb34316
Mahmoud AbdElMaguid <mahmoudabdelmaguid@cloud.upwork.com>
2022-01-20 21:58:56 +0200

    fix: remove extra div wrapper around LineTabPanel & fix storybook docs

--- src/components/line-tabs/LineTabs.stories.jsx
+++ src/components/line-tabs/LineTabs.stories.jsx
@@ -1,52 +1,11 @@
 import React, { useState } from "react";
+import {IconStarOutlined} from "../../icons";
 import { Button } from "../button";
 import { LineTabs, LineTabPanel } from "./LineTabs";
-import { IconStarOutlined } from "../../icons/IconStarOutlined";
-
-function LineTabsShowcase() {
-  const [currentTab, setCurrentTab] = useState(0);
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        padding: "32px 60px",
-        flexWrap: "wrap",
-        backgroundColor: "var(--ui-01)",
-        boxShadow: "0 10px 30px 0 rgba(89, 106, 122, 0.11)",
-        borderRadius: "5px",
-      }}
-    >
-      <div style={{ width: "100%" }}>
-        <LineTabs
-          initialValue={currentTab}
-          onChange={(newValue) => {
-            if (newValue != currentTab) {
-              setCurrentTab(newValue);
-            }
-          }}
-        >
-          <LineTabPanel title="Tab 1" icon={<IconStarOutlined></IconStarOutlined>}>
-            This is the first panelThis is the first panelThis is the first panelThis is the first panelThis is the
-            first panel
-          </LineTabPanel>
-          <LineTabPanel title="Tab 2">This is the second panel</LineTabPanel>
-          <LineTabPanel title="Tab 3">This is the third panel</LineTabPanel>
-        </LineTabs>
-        <br />
-        <p>Current tab: {currentTab}</p>
-        <br />
-        <Button size="small" onClick={() => setCurrentTab((currentTab + 1) % 3)}>
-          Change to next tab
-        </Button>
-      </div>
-    </div>
-  );
-}
 
 export default {
   title: "Components/Navigation/LineTabs",
-  component: LineTabsShowcase,
+  component: LineTabs,
   parameters: {
     design: {
       type: "figma",
@@ -55,7 +14,42 @@ export default {
   },
 };
 
-const Template = (args) => <LineTabsShowcase {...args} />;
+const Template = (args) => {
+  const [currentTab, setCurrentTab] = useState(0);
+  
+  return (
+      <div
+          style={{
+            display: "flex",
+            padding: "32px 60px",
+            flexWrap: "wrap",
+            backgroundColor: "var(--ui-01)",
+            boxShadow: "0 10px 30px 0 rgba(89, 106, 122, 0.11)",
+            borderRadius: "5px",
+          }}
+      >
+        <div style={{ width: "100%" }}>
+          <LineTabs
+              initialValue={currentTab}
+              onChange={(newValue) => setCurrentTab(newValue)}
+          >
+            <LineTabPanel title="Tab 1" icon={<IconStarOutlined/>}>
+              This is the first panelThis is the first panelThis is the first panelThis is the first panelThis is the
+              first panel
+            </LineTabPanel>
+            <LineTabPanel title="Tab 2">This is the second panel</LineTabPanel>
+            <LineTabPanel title="Tab 3">This is the third panel</LineTabPanel>
+          </LineTabs>
+          <br />
+          <p>Current tab: {currentTab}</p>
+          <br />
+          <Button size="small" onClick={() => setCurrentTab((currentTab + 1) % 3)}>
+            Change to next tab
+          </Button>
+        </div>
+      </div>
+  );
+}
 
 export const Default = Template.bind({});
 Default.args = {};
