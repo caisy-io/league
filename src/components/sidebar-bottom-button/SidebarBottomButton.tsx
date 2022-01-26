17c70c8c425dac5bca3aadb0a0017b7f4f78a6c2
Moritz Gruber <m.gruber@cyclus.digital>
2022-01-26 11:52:47 +0100

    chore: added on click
    chore: added on click

--- src/components/sidebar-bottom-button/SidebarBottomButton.tsx
+++ src/components/sidebar-bottom-button/SidebarBottomButton.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import {SSidebarBottomButton } from './styles/SSidebarBottomButton';
 
 interface ISidebarBottomButton{
   active?: boolean;
+  onClick?: (e: MouseEventHandler<HTMLButtonElement>) => void;
 }
 
-export const SidebarBottomButton: React.FC<ISidebarBottomButton> = ({active, children}) => {
+export const SidebarBottomButton: React.FC<ISidebarBottomButton> = ({onClick, active, children}) => {
   return (
-    <SSidebarBottomButton active={active}>
+    <SSidebarBottomButton active={active} onClick={onClick}>
       {children}
     </SSidebarBottomButton>
   )
