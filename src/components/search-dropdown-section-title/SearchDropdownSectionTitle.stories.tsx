38d674cf18afc63004828ac80b2d46fabc6cc98f
steiningermona <m.steininger@cyclus.digital>
2022-01-26 11:16:27 +0100

    finish title

--- src/components/search-dropdown-section-title/SearchDropdownSectionTitle.stories.tsx
+++ src/components/search-dropdown-section-title/SearchDropdownSectionTitle.stories.tsx
@@ -5,28 +5,9 @@ export default {
   title: `Components/SearchDropdownSectionTitle`,
   component: SearchDropdownSectionTitleDemo,
   argTypes: {
-    boolean: {
-      description: "An example boolean argument type",
-      control: { type: "boolean" },
-      table: {
-        defaultValue: {
-          summary: false,
-        },
-      },
-    },
     text: {
       description: "An example text argument type",
       control: { type: "text" },
-    },
-    select: {
-      description: "An example select argument type",
-      options: ["option1", "option2", "option2"],
-      control: { type: "select" },
-      table: {
-        defaultValue: {
-          summary: "option1",
-        },
-      },
     },
   },
 };
@@ -34,10 +15,10 @@ export default {
 function SearchDropdownSectionTitleDemo({ boolean, text, select }) {
   return <SearchDropdownSectionTitle>
     {text}
-    </SearchDropdownSectionTitle>
+  </SearchDropdownSectionTitle>
 }
 
-const Template = (args) => < SearchDropdownSectionTitleDemo {...args } />;
+const Template = (args) => < SearchDropdownSectionTitleDemo {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
