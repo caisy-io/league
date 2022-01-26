ecc28ec0568c0415acc9284347405b020ac270f0
steiningermona <m.steininger@cyclus.digital>
2022-01-26 09:53:08 +0100

    init floating search input component

---
+++ src/components/floating-search-input/SearchIcon.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function SearchIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20">
+      <circle
+        cx="9.216"
+        cy="9.216"
+        r="5.885"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      ></circle>
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M16.669 16.67l-3.293-3.294"
+      ></path>
+    </svg>
+  );
+}
+
+export default SearchIcon;
