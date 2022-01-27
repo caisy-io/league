76dbe3e78c8260056cdf971e9f7546cd3d93774b
Moritz Gruber <m.gruber@cyclus.digital>
2022-01-27 13:04:04 +0100

    chore: fix border radiys
    chore: fix border radiys

--- src/components/dropdown-card/styles/SDropdownCard.tsx
+++ src/components/dropdown-card/styles/SDropdownCard.tsx
@@ -3,7 +3,7 @@ import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from '../../../consta
 
 const Bronze = css`
 background-color: var(--ui-01);
-border-radius: 12px;
+border-radius: 8px;
 box-shadow: var(--box-shadow-light-pop-elevation);
 width: 320px;
 `;
