5558166ec3cdf3fe2cc71127660812399bd6a0e8
Moritz Gruber <m.gruber@cyclus.digital>
2022-02-13 10:22:55 +0100

    chore: ignore enter
    chore: ignore enter

--- src/components/floating-search-editable-text/FloatingSearchEditableText.tsx
+++ src/components/floating-search-editable-text/FloatingSearchEditableText.tsx
@@ -1,4 +1,4 @@
-import React, {  forwardRef, MouseEventHandler,  useState } from "react";
+import React, { forwardRef, MouseEventHandler, useEffect, useState } from "react";
 import { SFloatingSearchEditableText } from "./styles/SFloatingSearchEditableText";
 import { SFloatingSearchEditableTextWrapper } from "./styles/SFloatingSearchEditableTextWrapper";
 
@@ -9,41 +9,67 @@ interface IFloatingSearchEditableText {
   onClick?: (e: MouseEventHandler<HTMLDivElement>) => void;
 }
 
-export const FloatingSearchEditableText = forwardRef<HTMLInputElement, IFloatingSearchEditableText>(({ value, onClick, onChange }, inputRef) => {
-  const [active, setActive] = useState(false);
+export const FloatingSearchEditableText = forwardRef<HTMLInputElement, IFloatingSearchEditableText>(
+  ({ value, onClick, onChange }, inputRef) => {
+    const [active, setActive] = useState(false);
 
-  const handleClick = (e) => {
-    if(inputRef && (inputRef as any ).current){
-      (inputRef as any ).current.focus();
-    }
-    if(onClick){
-      onClick(e)
-    }
-  };
+    // const [value, setValue] = useState(undefined);
 
-  const handleFocus = () => {
-    setActive(true);
-  };
+    const handleClick = (e) => {
+      if (inputRef && (inputRef as any).current) {
+        (inputRef as any).current.focus();
+      }
+      if (onClick) {
+        onClick(e);
+      }
+    };
 
-  const onBlur = (e) => {
-    if(onChange){
-      onChange(e.currentTarget.textContent);
-    }
-  };
+    useEffect(() => {
+      const el = (inputRef as any).current;
+      if (el) {
+        const updateValue = (e) => {
+          if (e.inputType === "insertText" && e.data == null) {
+            // console.log(`ignore`, e);
+            e.preventDefault();
+            e.stopPropagation();
+            if(onChange){
+              onChange(`${e.target.innerText || ""}`.replace(/\n/g, ''));
+            }
+            (inputRef as any).current.blur();
+          } else {
+            // console.log(`truty`, e);
+          }
+        };
+        el.addEventListener("input", updateValue);
+      }
+    }, [inputRef && (inputRef as any).current]);
 
-  return (
-    <SFloatingSearchEditableTextWrapper active={active} onClick={handleClick}>
-      <SFloatingSearchEditableText
-        ref={inputRef}
-        role="textbox" 
-        contenteditable
-        suppressContentEditableWarning={true}
-        contentEditable
-        onFocus={handleFocus}
-        onBlur={onBlur}
-      >
-        {value}
-      </SFloatingSearchEditableText>
-    </SFloatingSearchEditableTextWrapper>
-  );
-});
+    const handleFocus = () => {
+      setActive(true);
+    };
+
+    const onBlur = (e) => {
+      if (onChange) {
+        console.log(` onChange`, );
+        onChange(e.currentTarget.textContent.replace(/\n/g, ''));
+      }
+    };
+
+    return (
+      <SFloatingSearchEditableTextWrapper active={active} onClick={handleClick}>
+        <SFloatingSearchEditableText
+          ref={inputRef}
+          role="textbox"
+          contenteditable
+          suppressContentEditableWarning={true}
+          contentEditable
+          onFocus={handleFocus}
+          onBlur={onBlur}
+          wrap={"off"}
+        >
+          {value}
+        </SFloatingSearchEditableText>
+      </SFloatingSearchEditableTextWrapper>
+    );
+  },
+);
