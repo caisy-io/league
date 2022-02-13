289205cdb26d9c6910b551103cbeccea07a8683f
Moritz Gruber <m.gruber@cyclus.digital>
2022-02-13 10:31:44 +0100

    chore: remove comment s
    chore: remove comment s

--- src/components/floating-search-editable-text/FloatingSearchEditableText.tsx
+++ src/components/floating-search-editable-text/FloatingSearchEditableText.tsx
@@ -13,8 +13,6 @@ export const FloatingSearchEditableText = forwardRef<HTMLInputElement, IFloating
   ({ value, onClick, onChange }, inputRef) => {
     const [active, setActive] = useState(false);
 
-    // const [value, setValue] = useState(undefined);
-
     const handleClick = (e) => {
       if (inputRef && (inputRef as any).current) {
         (inputRef as any).current.focus();
@@ -28,19 +26,18 @@ export const FloatingSearchEditableText = forwardRef<HTMLInputElement, IFloating
       const el = (inputRef as any)?.current;
       if (el) {
         const updateValue = (e) => {
+          // this will handle a enter key press inside the editable content container
           if (e.inputType === "insertText" && e.data == null) {
-            // console.log(`ignore`, e);
             e.preventDefault();
             e.stopPropagation();
             if(onChange){
               onChange(`${e.target.innerText || ""}`.replace(/\n/g, ''));
             }
             (inputRef as any).current.blur();
-          } else {
-            // console.log(`truty`, e);
           }
         };
         el.addEventListener("input", updateValue);
+        return () => el && el.removeEventListener("input", updateValue)
       }
     }, [inputRef && (inputRef as any)?.current]);
 
@@ -50,7 +47,6 @@ export const FloatingSearchEditableText = forwardRef<HTMLInputElement, IFloating
 
     const onBlur = (e) => {
       if (onChange) {
-        console.log(` onChange`, );
         onChange(e.currentTarget.textContent.replace(/\n/g, ''));
       }
     };
