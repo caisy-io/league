fe5e295b1944e078cc2e61a24e627304f9ae93cf
steiningermona <m.steininger@cyclus.digital>
2022-02-15 11:10:11 +0100

    add right onclick to menu list item

--- src/components/organization-menu-list-item/OrganizationMenuListItem.tsx
+++ src/components/organization-menu-list-item/OrganizationMenuListItem.tsx
@@ -22,7 +22,7 @@ export interface IOrganizationMenuListItemProps {
   previewProps?: IPreview;
   children?;
   disabled?: boolean;
-  onClick?: any;
+  onClick?: () => void;
 }
 
 const SFlexListItem = styled(SFlex)`
