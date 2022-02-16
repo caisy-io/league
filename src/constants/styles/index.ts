9d4aae2021030d8be966b7924f8a47f59eb24e37
MAHMOUD <mahmoud.taha.dev@gmail.com>
2022-02-16 18:23:00 +0200

    update exports

--- src/constants/styles/index.ts
+++ src/constants/styles/index.ts
@@ -3,3 +3,4 @@ export * from "./design-tokens/fonts/CSSTypographies";
 export {CSSScrollbarStyles} from "./scrollbar-styles";
 export * from "./mediaquerys";
 export * from "./z-index-numbers";
+export {CSSProgressiveCaption01Semibold} from "./design-tokens/fonts/CSSTypographies"
