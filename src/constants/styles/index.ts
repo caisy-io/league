8ae3abfc3a1e3ac4be238cbf320dd0f7429f4ae1
Moritz Gruber <m.gruber@cyclus.digital>
2022-02-17 10:14:32 +0100

    chore: remove unnedded export
    chore: remove unnedded export

--- src/constants/styles/index.ts
+++ src/constants/styles/index.ts
@@ -3,4 +3,3 @@ export * from "./design-tokens/fonts/CSSTypographies";
 export {CSSScrollbarStyles} from "./scrollbar-styles";
 export * from "./mediaquerys";
 export * from "./z-index-numbers";
-export {CSSProgressiveCaption01Semibold} from "./design-tokens/fonts/CSSTypographies"
