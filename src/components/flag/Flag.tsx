22e4983642cc26e23bb889e27867f877cb6ae42a
Andrijana Radic <a.radic@cyclus.digital>
2022-02-22 14:20:37 +0100

    wip: still try to use correctly

--- src/components/flag/Flag.tsx
+++ src/components/flag/Flag.tsx
@@ -3,9 +3,15 @@ import { SFlags } from "./styles/SFlags";
 import { IFlag } from "./types";
 
 export const Flag: FC<IFlag> = ({ countryCode, size }) => {
+  const CountryFlag = countryCode && (<img src={require(`./flags/round/${countryCode}.svg`).default}/>)
+
   return (
-    <SFlags size={size}>
-      {countryCode && require(`./flags/round/${countryCode}.svg`)}
-    </SFlags>
+    <>
+      {CountryFlag && (
+        <SFlags size={size}>
+          {CountryFlag}
+        </SFlags>
+      )}
+    </>
   );
 };
