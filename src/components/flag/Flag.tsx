10c7c4fc8c18a593c7bafb69b7f2a59c4bdb6ef8
Andrijana Radic <a.radic@cyclus.digital>
2022-02-22 13:11:30 +0100

    refactor: flags

--- src/components/flag/Flag.tsx
+++ src/components/flag/Flag.tsx
@@ -1,16 +1,15 @@
 import React, { FC } from "react";
-import * as Flags from "./flags";
 import { SFlags } from "./styles/SFlags";
 import { IFlag } from "./types";
 
 export const Flag: FC<IFlag> = ({ countryCode, size }) => {
-  const CountryFlag = countryCode && Flags[countryCode];
+  const CountryFlag = countryCode && <iframe src={require(`./flag/round/${countryCode}.svg`)} />
 
   return (
     <>
       {CountryFlag && (
         <SFlags size={size}>
-          <CountryFlag />
+          {CountryFlag}
         </SFlags>
       )}
     </>
