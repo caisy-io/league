9d88a8549a12b4e5f12603a629edbc4c3d8b251b
steiningermona <m.steininger@cyclus.digital>
2022-02-22 16:03:38 +0100

    add empty state

--- src/components/organization-select-menu/OrganizationSelectMenuItem.tsx
+++ src/components/organization-select-menu/OrganizationSelectMenuItem.tsx
@@ -5,19 +5,29 @@ import { SOrganizationSelectMenuItem } from "./styles/SOrganizationSelectMenuIte
 interface IOrganizationSelectMenuItem {
   logoAssetUrl?: string;
   name: string;
+  type?: string;
 }
 
+/* GET PLACEHOLDER LETTERS */
+const getPlaceholderLettersByName = (name: string) => {
+  const singleWords = name.split(" ");
+  if (singleWords.length == 1) {
+    return singleWords[0].charAt(0).toUpperCase();
+  }
+  return singleWords[0].charAt(0).toUpperCase() + singleWords[1].charAt(0).toUpperCase();
+};
+
 export const OrganizationSelectMenuItem: React.FC<{
-  menuItem: IOrganizationSelectMenuItem,
-  typeOrganization?: boolean;
+  menuItem?: IOrganizationSelectMenuItem
 }> = ({ ...props }) => {
   return props.menuItem ? (
     props.menuItem.logoAssetUrl ? (
-      <SOrganizationSelectMenuItem typeOrganization={props.typeOrganization}>
+      <SOrganizationSelectMenuItem typeOrganization={props.menuItem.type}>
         <Img resolution={1440} src={props.menuItem.logoAssetUrl} />
       </SOrganizationSelectMenuItem>
     ) : typeof props.menuItem.name === "string" ? (
-      <SOrganizationSelectMenuItem typeOrganization={props.typeOrganization}>{props.menuItem.name.slice(0, 2).toUpperCase()}</SOrganizationSelectMenuItem>
+      <SOrganizationSelectMenuItem typeOrganization={props.menuItem.type}>
+        {getPlaceholderLettersByName(props.menuItem.name)}</SOrganizationSelectMenuItem>
     ) : null
-  ) : null;
+  ) : <div>HI</div>;
 };
