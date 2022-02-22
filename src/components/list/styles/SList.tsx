cc2f9b3ec532ed51ae9b9a44820b41f67e011432
Moritz Gruber <m.gruber@cyclus.digital>
2022-02-22 19:17:37 +0100

    chore: update scrollbar injetced stypes
    chore: update scrollbar injetced stypes

--- src/components/list/styles/SList.tsx
+++ src/components/list/styles/SList.tsx
@@ -1,25 +1,31 @@
 import styled, { css } from "styled-components";
-import { CSSScrollbarStyles } from "../../../constants/styles";
 import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles/mediaquerys";
 
-interface ISList {}
-
-const Bronze = css<ISList>`
+const Bronze = css`
   background-color: var(--ui-01);
-
-  div {
-    ${CSSScrollbarStyles}
-    overflow-x: hidden;
+  div::-webkit-scrollbar {
+    background-color: var(--ui-01);
+    border-right: 1px solid var(--ui-03);
+    border-left: 1px solid var(--ui-03);
+    width: 0.75rem;
+    box-sizing: border-box;
+  }
+  div::-webkit-scrollbar-thumb {
+    border: 4px solid rgba(0, 0, 0, 0);
+    background-clip: padding-box;
+    -webkit-border-radius: 49px;
+    background-color: var(--interactional-secondary-08);
+    width: 4px;
   }
 `;
 
-const Silver = css<ISList>``;
+const Silver = css``;
 
-const Gold = css<ISList>``;
+const Gold = css``;
 
-const Platinum = css<ISList>``;
+const Platinum = css``;
 
-const Diamond = css<ISList>``;
+const Diamond = css``;
 
 export const SList = styled.ul`
   ${Bronze}
