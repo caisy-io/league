999a48ac2777ec89b8cfd87f540964259d46f6e9
Andrijana Radic <a.radic@cyclus.digital>
2022-02-22 13:59:55 +0100

    fix: this should fix the cannot find module error

--- src/components/flag/Flag.tsx
+++ src/components/flag/Flag.tsx
@@ -3,7 +3,7 @@ import { SFlags } from "./styles/SFlags";
 import { IFlag } from "./types";
 
 export const Flag: FC<IFlag> = ({ countryCode, size }) => {
-  const CountryFlag = countryCode && <iframe src={require(`./flag/round/${countryCode}.svg`)} />
+  const CountryFlag = countryCode && <iframe src={require(`./flags/round/${countryCode}.svg`)} />
 
   return (
     <>
