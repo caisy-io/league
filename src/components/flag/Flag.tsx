6267ad5fd3726d25408fe3cfe00229f0a96599e9
Andrijana Radic <a.radic@cyclus.digital>
2022-02-22 14:15:36 +0100

    wip: try to use flags correctly

--- src/components/flag/Flag.tsx
+++ src/components/flag/Flag.tsx
@@ -3,15 +3,9 @@ import { SFlags } from "./styles/SFlags";
 import { IFlag } from "./types";
 
 export const Flag: FC<IFlag> = ({ countryCode, size }) => {
-  const CountryFlag = countryCode && <iframe src={require(`./flags/round/${countryCode}.svg`)} />
-
   return (
-    <>
-      {CountryFlag && (
-        <SFlags size={size}>
-          {CountryFlag}
-        </SFlags>
-      )}
-    </>
+    <SFlags size={size}>
+      {countryCode && require(`./flags/round/${countryCode}.svg`)}
+    </SFlags>
   );
 };
