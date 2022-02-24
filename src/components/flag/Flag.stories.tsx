a94599d68943f5aba9525fe4b62186a8011b1af7
Andrijana Radic <a.radic@cyclus.digital>
2022-02-24 10:31:45 +0100

    fix: styling for country flags

--- src/components/flag/Flag.stories.tsx
+++ src/components/flag/Flag.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
-import Flag from "./Flag";
+import { Flag } from "./Flag";
 
 export default {
   title: "Components/Style Guide/Flag",
@@ -22,13 +22,21 @@ export default {
 } as ComponentMeta<typeof Flag>;
 
 function FlagDemo({ ...args }) {
-  return <Flag countryCode={args.countryCode} {...args} ></Flag>;
+  // TODO Default webpack config of Storybook contains a svg loader that does not work as we want for svgs
+  // TODO Disable default behaviour and use svgr instead (see https://github.com/storybookjs/storybook/issues/6188#issuecomment-487705465, https://medium.com/@derek_19900/config-storybook-4-to-use-svgr-webpack-plugin-22cb1152f004)
+  //const CountryFlag = require(`./flags/round/${args.countryCode}.svg`).default;
+  // we can modify the final webpack config of storybook in .storybook/main.js (see https://storybook.js.org/docs/react/configure/webpack)
+
+  return (
+    <Flag {...args}>
+      
+    </Flag>
+  );
 }
 
 const Template: ComponentStory<typeof Flag> = (args) => <FlagDemo  {...args} />;
 
 export const Flag_default = Template.bind({});
 Flag_default.args = {
-  children: "Ac",
-  countryCode: "ac",
+  countryCode: "en-GB",
 };
