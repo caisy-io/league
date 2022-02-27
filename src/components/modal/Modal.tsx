fa5d6f957747fe2cb3025adb6aae1ed446890b1e
Moritz Gruber <m.gruber@cyclus.digital>
2022-02-27 11:10:10 +0100

    chore: remove min with from modal and
    chore: remove min with from modal and give it style overwrite

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -3,13 +3,15 @@ import { createPortal } from "react-dom";
 import Stackable from "../stackable";
 import { SModal } from "./styles/SModal";
 import { SModalBackground } from "./styles/SModalBackground";
+import {ThemedCssFunction} from "styled-components";
 
 interface IModal {
   visible: boolean;
   onClose: (payload: any) => any;
+  styleOverwrite?: ThemedCssFunction<any>;
 }
 
-export const Modal: React.FC<IModal> = ({ ...props }) => {
+export const Modal: React.FC<IModal> = ({ styleOverwrite, ...props }) => {
   const [docBody, setDocBody] = React.useState<any>(null);
   React.useEffect(() => {
     setDocBody(document.body);
@@ -19,7 +21,7 @@ export const Modal: React.FC<IModal> = ({ ...props }) => {
     ? createPortal(
         <Stackable>
           <SModalBackground onClick={props.onClose}>
-            <SModal onClick={(e) => e.stopPropagation()}>{props.children}</SModal>
+            <SModal styleOverwrite={styleOverwrite} onClick={(e) => e.stopPropagation()}>{props.children}</SModal>
           </SModalBackground>
         </Stackable>,
         docBody,
