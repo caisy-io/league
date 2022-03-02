3ab333a746b495ff3e9209a43d70369386731e5e
Nicolas Shiken <nicolasshiken@gmail.com>
2022-03-02 09:22:27 -0300

    Update SSidebarBottomButton.tsx

--- src/components/sidebar-bottom-button/styles/SSidebarBottomButton.tsx
+++ src/components/sidebar-bottom-button/styles/SSidebarBottomButton.tsx
@@ -15,13 +15,13 @@ const Bronze = css<{active?: boolean}>`
   width: 100%;
   border: none;
   cursor: pointer;
-  ${CSSProgressivePrimaryButtons}
+  ${CSSProgressivePrimaryButtons};
   display: flex;
   flex-direction: row;
   gap: 8px;
   align-items: center;
   padding: 12px 16px;
-  min-height: 40px
+  min-height: 40px;
   background-color: var(--ui-01);
   color: var(--text-04);
   box-shadow: var(--box-shadow-sticked-bottom-ui);
