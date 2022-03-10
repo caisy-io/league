8031c3c73aee7cbc3b24daaefd1cc34a3c09f66b
Diego Martinez <diegomart_2000@yahoo.com>
2022-03-10 11:46:30 -0300

    Export ui components

--- src/components/index.ts
+++ src/components/index.ts
@@ -116,3 +116,5 @@ export * from "./micro-tab-switch";
 export * from "./slider";
 export * from "./language-format-input-dropdown";
 export * from "./input-field";
+export * from "./block-editor-sidebar";
+export * from "./language-toggle-list-item";
