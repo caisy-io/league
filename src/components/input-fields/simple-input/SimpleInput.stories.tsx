4984f364cc73952533cc2663041bad05612d18ff
Mahmoud AbdElMaguid <mahmoudabdelmaguid@cloud.upwork.com>
2022-03-23 14:57:48 +0200

    chore: add SimpleInputWrapper and add type prop to SimpleInput

--- src/components/input-fields/simple-input/SimpleInput.stories.tsx
+++ src/components/input-fields/simple-input/SimpleInput.stories.tsx
@@ -1,8 +1,7 @@
 import React from "react";
+import {TranslationBadge} from "../TranslationBadge";
 import { SimpleInput } from "./SimpleInput";
-import { STranslationBadge } from "../styles/STranslationBadge";
 import { IconStarOutlined } from "../../../icons";
-import { Flag } from "../../flag/Flag";
 
 function SimpleInputDemo({ ...args }) {
   const [value, setValue] = React.useState("");
@@ -10,28 +9,21 @@ function SimpleInputDemo({ ...args }) {
     setValue(e.target.value);
   };
 
-  const TranslationBadge = () => {
-    return (
-      <STranslationBadge>
-        <Flag countryCode="de" size={16} />
-        DE
-      </STranslationBadge>
-    );
-  };
 
   return (
     <SimpleInput
-      translationBadge={args.withTranslationBadge && <TranslationBadge />}
+      translationBadge={args.withTranslationBadge && <TranslationBadge countryCode={'de'}/>}
       required={args.required}
       onChange={onChange}
       value={value}
       disabled={args.state === "locked"}
       leftIcon={args.withLeftIcon && <IconStarOutlined size={20} />}
       rightIcon={args.withRightIcon && <IconStarOutlined size={20} />}
-      errors={args.state === "error" && ["Error message"]}
+      errors={args.state === "error" ? ["Error message"] : undefined}
       placeholder="Placeholder"
       label={args.label}
       state={args.state}
+      type={args.type}
     />
   );
 }
@@ -51,7 +43,7 @@ export default {
       },
     },
     required: {
-      description: "Marks the input with a required inidicator",
+      description: "Marks the input with a required indicator",
       control: { type: "boolean" },
       table: {
         defaultValue: {
@@ -67,6 +59,7 @@ const Template = ({ ...args }) => <SimpleInputDemo {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   label: "This is a label",
+  type: "text",
   state: "error",
   required: true,
   withLeftIcon: false,
--- src/components/input-fields/simple-input/SimpleInputWrapper.tsx
+++ src/components/input-fields/simple-input/SimpleInputWrapper.tsx
@@ -1,111 +1,64 @@
 import React, { FC, KeyboardEvent, ReactNode, useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { useDimensions } from "../../..";
 import { SErrorMessage } from "../styles";
 import { SLabel } from "../styles";
-import { SSimpleInput } from "./styles/SSimpleInput";
 import { SSimpleInputIconWrapper } from "./styles/SSimpleInputIconWrapper";
 import { SSimpleInputInsideContainer } from "./styles/SSimpleInputInsideContainer";
 import { SSimpleInputOutsideContainer } from "./styles/SSimpleInputOutsideContainer";
 import { SSimpleInputRequiredIndicator } from "./styles/SSimpleInputRequiredIndicator";
 import { SSimpleInputRequiredIndicatorContainer } from "./styles/SSimpleInputRequiredIndicatorContainer";
 import { SSimpleInputRightWrapper } from "./styles/SSimpleInputRightIcon";
-import { SSimpleInputTextWidth } from "./styles/SSimpleInputTextWidth";
 import { SSimpleInputWrapper } from "./styles/SSimpleInputWrapper";
 
 type TSimpleInputState = "success" | "error" | "default" | "locked";
 
-interface ISimpleInput {
+export interface ISimpleInput {
   label?: string | ReactNode;
   errors?: string[];
   state?: TSimpleInputState;
   translationBadge?: JSX.Element;
   required?: boolean;
-  onChange?: (e: any) => void;
-  value?: string;
-  placeholder?: string;
   onFocus?: (e: InputEvent) => void;
   onBlur?: (e: InputEvent) => void;
-  disabled?: boolean;
   leftIcon?: JSX.Element;
   rightIcon?: JSX.Element;
   onKeyUp?: (e: KeyboardEvent<HTMLInputElement>) => void;
   autoComplete?: string;
   autoFocus?: boolean;
-  type?: Pick<HTMLInputElement, 'type'>
 }
 
-export const SimpleInput: FC<ISimpleInput> = ({
+export const SimpleInputWrapper: FC<ISimpleInput> = ({
   state,
   required,
-  value,
-  onChange,
-  placeholder,
   onFocus,
   onBlur,
   label,
   errors,
   translationBadge,
-  disabled,
   leftIcon,
   rightIcon,
-  onKeyUp,
-  type='text'
+  children
 }) => {
   const [active, setActive] = useState(false);
   const [hover, setHover] = useState(false);
-
-  const inputRef = useRef<HTMLInputElement>();
-  const spanRef = useRef<HTMLSpanElement>();
-
-  const handleClick = useCallback(() => {
-    inputRef.current?.focus();
-  }, [inputRef.current?.focus]);
-
-  const handleFocus = useCallback(
-    (e) => {
-      setActive(true);
-      onFocus?.(e);
-    },
-    [setActive, onFocus],
-  );
-
-  const handleBlur = useCallback(
-    (e) => {
-      setActive(false);
-      onBlur?.(e);
-    },
-    [setActive, onBlur],
-  );
-
-  const [inputWidth, setInputWidth] = useState<number | undefined>(undefined);
-
-  const resizeInput = useCallback(() => {
-    let width: number | undefined = undefined;
-
-    if (!inputRef?.current?.value && placeholder) {
-      (spanRef.current as HTMLSpanElement).innerText = placeholder;
-      width = (spanRef.current?.scrollWidth as number) + 1;
-    }
-
-    setInputWidth(width);
-  }, [setInputWidth, placeholder, inputRef?.current?.value]);
-
-  useEffect(() => {
-    resizeInput();
-  }, [value, placeholder]);
 
   return (
     <SSimpleInputWrapper
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
+      onFocus={(e) => {
+        setActive(true)
+        onFocus?.(e);
+      }}
+      onBlur={(e) => {
+        setActive(false)
+        onBlur?.(e);
+      }}
       active={active}
       state={state}
-      onClick={handleClick}
     >
       <SSimpleInputOutsideContainer>
         <SSimpleInputIconWrapper>{leftIcon}</SSimpleInputIconWrapper>
         <SSimpleInputInsideContainer>
-          <SSimpleInputTextWidth ref={spanRef} />
           {label && (
             <SSimpleInputRequiredIndicatorContainer withLabel={true}>
               {required && <SSimpleInputRequiredIndicator />}
@@ -125,27 +78,11 @@ export const SimpleInput: FC<ISimpleInput> = ({
 
           {translationBadge}
 
-          <SSimpleInputRequiredIndicatorContainer width={inputWidth}>
-            {required && !inputRef?.current?.value && !label && (!errors || errors.length === 0) && (
+          <SSimpleInputRequiredIndicatorContainer>
+            {required && !label && (!errors || errors.length === 0) && (
               <SSimpleInputRequiredIndicator />
             )}
-            <SSimpleInput
-              width={inputWidth}
-              error={state === "error"}
-              locked={state === "locked"}
-              onChange={(e) => {
-                onChange?.(e);
-              }}
-              required={required}
-              ref={inputRef}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-              value={value || ""}
-              placeholder={placeholder}
-              disabled={disabled}
-              onKeyUp={onKeyUp}
-              type={type}
-            />
+            {children}
           </SSimpleInputRequiredIndicatorContainer>
         </SSimpleInputInsideContainer>
         <SSimpleInputRightWrapper>
