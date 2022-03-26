f9db757f9e781af0f8e75e990522c7724a09f56a
Moritz Gruber <m.gruber@cyclus.digital>
2022-03-26 21:08:20 +0100

    chore: added curser pointer
    chore: added curser pointer

--- src/components/component-card/styles/SComponentCard.tsx
+++ src/components/component-card/styles/SComponentCard.tsx
@@ -9,11 +9,12 @@ const Bronze = css`
   flex-direction: column;
   gap: 1rem;
   box-sizing: border-box;
-  &: hover{
+  cursor: pointer;
+  &:hover{
     background-color: var(--ui-01);
     box-shadow: 0px 8px 24px rgba(184, 184, 184, 0.24);
   }
-  &: active {
+  &:active {
     background-color: var(--ui-01);
     border: 1px solid #C3D1D9;
     box-shadow: none;
