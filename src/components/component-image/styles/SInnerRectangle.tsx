4c87207a946db3fa9c1f0390de265c04458f5e88
Moritz Gruber <m.gruber@cyclus.digital>
2022-03-28 16:30:13 +0200

    chore: update the styles
    chore: update the styles

--- src/components/component-image/styles/SInnerRectangle.tsx
+++ src/components/component-image/styles/SInnerRectangle.tsx
@@ -1,24 +1,27 @@
-import styled, { css } from 'styled-components';
-import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from '../../../constants/styles/mediaquerys';
+import styled, { css } from "styled-components";
+import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles/mediaquerys";
 
 export const CSSTypeGrid = css`
-    
-    border-radius: 0.25rem;
+  border-radius: 0.25rem;
 `;
 
 export const CSSTypeListItem = css`
-    border-radius: 0.25rem;
+  border-radius: 0.25rem;
 `;
 
 const Bronze = css`
-    background-color: var(--ui-01);
+  background-color: var(--ui-01);
+  width: 100%;
+  height: 100%;
+  img {
+    border-radius: 0.5rem;
+    ${(props) => (props.type == "grid" ? CSSTypeGrid : "")};
+    ${(props) => (props.type == "list-item" ? CSSTypeListItem : "")};
+  }
+  .lazyload-wrapper {
     width: 100%;
     height: 100%;
-    img{
-        border-radius: 0.5rem;
-        ${(props) => props.type == "grid" ? CSSTypeGrid : ""};
-        ${(props) => props.type == "list-item" ? CSSTypeListItem : ""};
-    }
+  }
 `;
 
 const Silver = css``;
@@ -32,8 +35,7 @@ const Diamond = css``;
 export const SInnerRectangle = styled.div`
   ${Bronze}
   ${MIN_SILVER`${Silver}`};
-  ${MIN_GOLD`${Gold}`}; 
+  ${MIN_GOLD`${Gold}`};
   ${MIN_PLATINUM`${Platinum}`};
   ${MIN_DIAMOND`${Diamond}`};
 `;
-
