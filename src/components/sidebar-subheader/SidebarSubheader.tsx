aa52d23fba24c987a4297e113774d5679813c59b
mahmoudtahadev <97168820+mahmoudtahadev@users.noreply.github.com>
2022-04-01 11:01:38 +0200

    update style (#333)
    Co-authored-by: MAHMOUD <mahmoud.taha.dev@gmail.com>

--- src/components/sidebar-subheader/SidebarSubheader.tsx
+++ src/components/sidebar-subheader/SidebarSubheader.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Button } from '../../components/button';
-import { IconChevronLeft } from '../../icons/IconChevronLeft';
-import { SButtonOverwrite } from './styles/SButtonOverwrite';
-import { SSidebarSubheader } from './styles/SSidebarSubheader';
-import { SSidebarSubheaderText } from './styles/SSidebarSubheaderText';
+import React from "react";
+import { Button } from "../../components/button";
+import { IconChevronLeft } from "../../icons/IconChevronLeft";
+import { SButtonOverwrite } from "./styles/SButtonOverwrite";
+import { SSidebarSubheader } from "./styles/SSidebarSubheader";
+import { SSidebarSubheaderText } from "./styles/SSidebarSubheaderText";
 
 interface ISidebarSubheader {
-  subheaderText?: string,
-  styleOverwrite?: string,
-  onClick?: () => void,
+  subheaderText?: string;
+  styleOverwrite?: string;
+  onClick?: () => void;
 }
 
 export const SidebarSubheader: React.FC<ISidebarSubheader> = ({ styleOverwrite, subheaderText, onClick }) => {
   return (
     <SSidebarSubheader styleOverwrite={styleOverwrite}>
-      <Button onClick={onClick} styleOverwrite={SButtonOverwrite} type="secondary" size="small"> <IconChevronLeft size={20} /> </Button>
-      <SSidebarSubheaderText>  <span>{subheaderText} </span></SSidebarSubheaderText>
+      <Button onClick={onClick} styleOverwrite={SButtonOverwrite} type="neutral" size="small">
+        {" "}
+        <IconChevronLeft size={20} />{" "}
+      </Button>
+      <SSidebarSubheaderText>
+        {" "}
+        <span>{subheaderText} </span>
+      </SSidebarSubheaderText>
     </SSidebarSubheader>
-  )
-}
+  );
+};
