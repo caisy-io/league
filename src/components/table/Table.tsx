362c315793d427e05e7db8f0b61def71c2954ad1
MAHMOUD <mahmoud.taha.dev@gmail.com>
2022-04-05 22:01:11 +0200

    fix bug

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -53,207 +53,196 @@ interface ITable {
   empty?: ReactNode;
 }
 
-export const Table: FC<ITable> = memo(
-  forwardRef(
-    (
-      {
-        dataSource,
-        tableOptions,
-        globalFilter,
-        itemSize,
-        isNextPageLoading,
-        loadNextPage,
-        hasNextPage,
-        loading,
-        emptyMessage,
-        style,
-        rowStyle,
-        onRowClick,
-        onHeaderClick,
-        columns,
-        renderAsFirstRow,
-        empty,
-      },
-      ref,
-    ) => {
-      const containerRef = useRef<any>({});
-      const headerRef = useRef<any>({});
-      const bodyRef = useRef<HTMLDivElement>();
-      const [innerColumns, setColumns] = useState<any[]>([]);
-
-      const { height: containerHeight } = useDimensions(containerRef);
-      const { height: headerHeight } = useDimensions(headerRef);
-      const height = containerHeight - headerHeight;
-
-      console.log("table rendered");
-      useEffect(() => {
-        columns &&
-          setColumns(
-            columns.map((column) => {
-              return !!column.renderItem
-                ? {
-                    Header: column.header,
-                    accessor: column.key,
-                    Cell: (cellProps) => (column.renderItem as any)(cellProps.cell.value, cellProps.cell.row.index),
-                    defaultCanSort: column.defaultCanSort,
-                    style: column.style,
-                  }
-                : {
-                    Header: column.header,
-                    accessor: column.key,
-                    defaultCanSort: column.defaultCanSort,
-                    style: column.style,
-                  };
-            }),
-          );
-      }, [columns]);
-
-      // Only load 1 page of items at a time.
-      // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
-      const loadMoreItems = isNextPageLoading ? () => {} : loadNextPage;
-
-      // Every row is loaded except for our loading indicator row.
-      // const isItemLoaded = (index) => !props.hasNextPage || index < props.dataSource.length;
-      if (!dataSource) {
-        return null;
-      }
-
-      const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, setGlobalFilter } = useTable(
-        {
-          columns: innerColumns,
-          data: dataSource,
-          ...(tableOptions ? tableOptions : {}),
-        },
-        useGlobalFilter,
-        useSortBy,
-      );
-
-      useEffect(() => {
-        setGlobalFilter(globalFilter);
-      }, [globalFilter]);
-
-      React.useEffect(() => {
-        console.log("rows");
-      }, [rows]);
-
-      const RenderRow = memo(({ data, index, style }: any) => {
-        console.log("rendered row");
-        const row = data[index];
-        if (row) {
-          prepareRow(row);
-          return (
-            <>
-              <STr
-                onClick={() => (!!onRowClick ? onRowClick(row) : () => {})}
-                key={index}
-                {...row.getRowProps({
-                  style: { ...style, rowStyle },
-                })}
-              >
-                {row.cells.map((cell, cellIndex) => {
-                  return (
-                    <STd
-                      key={cellIndex}
-                      style={{ textOverflow: "ellipsis", overflow: "hidden", display: "block", ...cell?.value?.style }}
-                      {...cell.getCellProps()}
-                    >
-                      {cell.render("Cell")}
-                    </STd>
-                  );
-                })}
-              </STr>
-            </>
-          );
-        }
-        return null;
-      }, areEqual);
-
-      const triggerLoadMoreItems = () => {
-        const table = bodyRef?.current;
-
-        if (!!table && table.scrollTop / (table.scrollHeight - table.clientHeight) > 0.8) {
-          hasNextPage && (loadMoreItems as any)();
-        }
-      };
-
-      const firstRowRef = useRef<HTMLDivElement>(null);
-
-      const onScroll = ({ scrollOffset }) => {
-        if (!!firstRowRef?.current) {
-          firstRowRef.current.style.transform = `translateY(-${scrollOffset * 2}px)`;
-          const height =
-            scrollOffset * 2 < firstRowRef.current.scrollHeight
-              ? firstRowRef.current.scrollHeight - scrollOffset * 2
-              : 0;
-
-          firstRowRef.current.style.height = height + "px";
-        }
-        debounce(() => triggerLoadMoreItems(), 160);
-      };
-
-      const TableWithRows = useMemo(() => {
-        console.log("%cTableWithRows", "color:red; font-size:20px");
-        return (
-          <FixedSizeList
-            onScroll={onScroll}
-            outerRef={bodyRef}
-            className="league-table"
-            height={height}
-            itemSize={itemSize}
-            itemData={rows}
-            itemCount={dataSource?.length || 0}
-            ref={ref}
-          >
-            {RenderRow}
-          </FixedSizeList>
-        );
-      }, [rows, dataSource]);
-
-      return (
-        <STable ref={containerRef} style={style} {...getTableProps()}>
-          <SThead ref={headerRef}>
-            {headerGroups.map((headerGroup, headerIndex) => (
-              <STr style={{ ...rowStyle }} key={headerIndex} {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column, columnIndex) => {
-                  return (
-                    <STh
-                      {...column.getHeaderProps(column.getSortByToggleProps())}
-                      onClick={() => onHeaderClick && onHeaderClick(column)}
-                      key={columnIndex}
-                      style={{ ...column.style }}
-                    >
-                      {column.render("Header")}
-                      {column.defaultCanSort !== false && column.isSorted ? (
-                        column.isSortedDesc == "DESC" ? (
-                          <IconAngleDown />
-                        ) : (
-                          <IconAngleUp />
-                        )
-                      ) : null}
-                    </STh>
-                  );
-                })}
-              </STr>
-            ))}
-          </SThead>
-          <STbody {...getTableBodyProps()}>
-            {loading ? (
-              <STableLoading height={height}>
-                <Spinner />
-              </STableLoading>
-            ) : dataSource?.length ? (
-              <>
-                {!!renderAsFirstRow && <div ref={firstRowRef}>{renderAsFirstRow}</div>}
-                {TableWithRows}
-              </>
-            ) : empty ? (
-              empty
-            ) : (
-              <Empty type="blueprint" title="" description={emptyMessage || "No data found."} />
-            )}
-          </STbody>
-        </STable>
-      );
+export const Table: FC<ITable> = forwardRef(
+  (
+    {
+      dataSource,
+      tableOptions,
+      globalFilter,
+      itemSize,
+      isNextPageLoading,
+      loadNextPage,
+      hasNextPage,
+      loading,
+      emptyMessage,
+      style,
+      rowStyle,
+      onRowClick,
+      onHeaderClick,
+      columns,
+      renderAsFirstRow,
+      empty,
     },
-  ),
+    ref,
+  ) => {
+    const containerRef = useRef<any>({});
+    const headerRef = useRef<any>({});
+    const bodyRef = useRef<HTMLDivElement>();
+    const [innerColumns, setColumns] = useState<any[]>([]);
+
+    const { height: containerHeight } = useDimensions(containerRef);
+    const { height: headerHeight } = useDimensions(headerRef);
+    const height = containerHeight - headerHeight;
+
+    useEffect(() => {
+      columns &&
+        setColumns(
+          columns.map((column) => {
+            return !!column.renderItem
+              ? {
+                  Header: column.header,
+                  accessor: column.key,
+                  Cell: (cellProps) => (column.renderItem as any)(cellProps.cell.value, cellProps.cell.row.index),
+                  defaultCanSort: column.defaultCanSort,
+                  style: column.style,
+                }
+              : {
+                  Header: column.header,
+                  accessor: column.key,
+                  defaultCanSort: column.defaultCanSort,
+                  style: column.style,
+                };
+          }),
+        );
+    }, [columns]);
+
+    // Only load 1 page of items at a time.
+    // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+    const loadMoreItems = isNextPageLoading ? () => {} : loadNextPage;
+
+    // Every row is loaded except for our loading indicator row.
+    // const isItemLoaded = (index) => !props.hasNextPage || index < props.dataSource.length;
+    if (!dataSource) {
+      return null;
+    }
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, setGlobalFilter } = useTable(
+      {
+        columns: innerColumns,
+        data: dataSource,
+        ...(tableOptions ? tableOptions : {}),
+      },
+      useGlobalFilter,
+      useSortBy,
+    );
+
+    useEffect(() => {
+      setGlobalFilter(globalFilter);
+    }, [globalFilter]);
+
+    const RenderRow = memo(({ data, index, style }: any) => {
+      const row = data[index];
+      if (row) {
+        prepareRow(row);
+        return (
+          <>
+            <STr
+              onClick={() => (!!onRowClick ? onRowClick(row) : () => {})}
+              key={index}
+              {...row.getRowProps({
+                style: { ...style, rowStyle },
+              })}
+            >
+              {row.cells.map((cell, cellIndex) => {
+                return (
+                  <STd
+                    key={cellIndex}
+                    style={{ textOverflow: "ellipsis", overflow: "hidden", display: "block", ...cell?.value?.style }}
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render("Cell")}
+                  </STd>
+                );
+              })}
+            </STr>
+          </>
+        );
+      }
+      return null;
+    }, areEqual);
+
+    const triggerLoadMoreItems = () => {
+      const table = bodyRef?.current;
+
+      if (!!table && table.scrollTop / (table.scrollHeight - table.clientHeight) > 0.8) {
+        hasNextPage && (loadMoreItems as any)();
+      }
+    };
+
+    const firstRowRef = useRef<HTMLDivElement>(null);
+
+    const onScroll = ({ scrollOffset }) => {
+      if (!!firstRowRef?.current) {
+        firstRowRef.current.style.transform = `translateY(-${scrollOffset * 2}px)`;
+        const height =
+          scrollOffset * 2 < firstRowRef.current.scrollHeight ? firstRowRef.current.scrollHeight - scrollOffset * 2 : 0;
+
+        firstRowRef.current.style.height = height + "px";
+      }
+      debounce(() => triggerLoadMoreItems(), 160);
+    };
+
+    const TableWithRows = useMemo(() => {
+      return (
+        <FixedSizeList
+          onScroll={onScroll}
+          outerRef={bodyRef}
+          className="league-table"
+          height={height}
+          itemSize={itemSize}
+          itemData={rows}
+          itemCount={dataSource?.length || 0}
+          ref={ref}
+        >
+          {RenderRow}
+        </FixedSizeList>
+      );
+    }, [rows, dataSource]);
+
+    return (
+      <STable ref={containerRef} style={style} {...getTableProps()}>
+        <SThead ref={headerRef}>
+          {headerGroups.map((headerGroup, headerIndex) => (
+            <STr style={{ ...rowStyle }} key={headerIndex} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, columnIndex) => {
+                return (
+                  <STh
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    onClick={() => onHeaderClick && onHeaderClick(column)}
+                    key={columnIndex}
+                    style={{ ...column.style }}
+                  >
+                    {column.render("Header")}
+                    {column.defaultCanSort !== false && column.isSorted ? (
+                      column.isSortedDesc == "DESC" ? (
+                        <IconAngleDown />
+                      ) : (
+                        <IconAngleUp />
+                      )
+                    ) : null}
+                  </STh>
+                );
+              })}
+            </STr>
+          ))}
+        </SThead>
+        <STbody {...getTableBodyProps()}>
+          {loading ? (
+            <STableLoading height={height}>
+              <Spinner />
+            </STableLoading>
+          ) : dataSource?.length ? (
+            <>
+              {!!renderAsFirstRow && <div ref={firstRowRef}>{renderAsFirstRow}</div>}
+              {TableWithRows}
+            </>
+          ) : empty ? (
+            empty
+          ) : (
+            <Empty type="blueprint" title="" description={emptyMessage || "No data found."} />
+          )}
+        </STbody>
+      </STable>
+    );
+  },
 );
