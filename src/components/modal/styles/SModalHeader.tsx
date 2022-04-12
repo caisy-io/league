35e56e35fcd69b90ba5413c88476c25eeee03a9d
Moritz Gruber <m.gruber@cyclus.digital>
2022-04-12 22:01:37 +0200

    Update SModalHeader.tsx

--- src/components/modal/styles/SModalHeader.tsx
+++ src/components/modal/styles/SModalHeader.tsx
@@ -2,14 +2,13 @@ import { CSSProperties } from "react";
 import styled, { css } from "styled-components";
 import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles";
 
-const Bronze = css<{ styleOverwrite?: CSSProperties }>`
+const Bronze = css`
   padding: 16px 24px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-bottom: 1px solid var(--ui-03);
 
-  ${({ styleOverwrite }) => styleOverwrite && styleOverwrite};
 `;
 
 const Silver = css``;
@@ -20,10 +19,11 @@ const Platinum = css``;
 
 const Diamond = css``;
 
-export const SModalHeader = styled.div`
+export const SModalHeader = styled.div<{ styleOverwrite?: CSSProperties }>`
   ${Bronze}
   ${MIN_SILVER`${Silver}`};
   ${MIN_GOLD`${Gold}`};
   ${MIN_PLATINUM`${Platinum}`};
   ${MIN_DIAMOND`${Diamond}`};
+  ${({ styleOverwrite }) => styleOverwrite && styleOverwrite};
 `;
