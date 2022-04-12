b808c5496439cfad15d2f2f090ee67ef06969f31
MAHMOUD <mahmoud.taha.dev@gmail.com>
2022-04-12 12:42:44 +0200

    fix bug

--- src/components/empty/EmptyTable.tsx
+++ src/components/empty/EmptyTable.tsx
@@ -1,10 +1,13 @@
 import React, { FC, ReactNode } from "react";
 import EmptyBannerImage from "./EmptyBannerImage";
-import EmptyTableRowsImage from "./EmptyTableRowsImage";
+import { SEmptyTable } from "./styles/SEmptyTable";
 import { SEmptyTableBanner } from "./styles/SEmptyTableBanner";
 import { SEmptyTableBannerDescription } from "./styles/SEmptyTableBannerDescription";
 import { SEmptyTableBannerTitle } from "./styles/SEmptyTableBannerTitle";
-import { SEmptyTableRows } from "./styles/SEmptyTableRows";
+import { SEmptyTableCell } from "./styles/SEmptyTableCell";
+import { SEmptyTableCellContent } from "./styles/SEmptyTableCellContent";
+import { SEmptyTableRow } from "./styles/SEmptyTableRow";
+import { SEmptyTableSkeleton } from "./styles/SEmptyTableSkeleton";
 
 interface IEmptyTable {
   title: string | ReactNode;
@@ -13,15 +16,67 @@ interface IEmptyTable {
 
 export const EmptyTable: FC<IEmptyTable> = ({ title, description }) => {
   return (
-    <>
-      <SEmptyTableRows>
-        <EmptyTableRowsImage />
-        <SEmptyTableBanner>
-          <EmptyBannerImage />
-          <SEmptyTableBannerTitle>{title}</SEmptyTableBannerTitle>
-          <SEmptyTableBannerDescription>{description}</SEmptyTableBannerDescription>
-        </SEmptyTableBanner>
-      </SEmptyTableRows>
-    </>
+    <SEmptyTable>
+      {[...Array(5)].map((_, index) => (
+        <SEmptyTableRow key={`emptyAssets${index}`}>
+          <SEmptyTableCell
+            styleOverwrite={{
+              width: 48,
+              flex: "none",
+              boxShadow: "4px 0px 8px rgba(0, 0, 0, 0.04)",
+              zIndex: 2,
+              position: "relative",
+            }}
+          >
+            <SEmptyTableCellContent>
+              <SEmptyTableSkeleton />
+            </SEmptyTableCellContent>
+          </SEmptyTableCell>
+          <SEmptyTableCell
+            styleOverwrite={{
+              width: "84px",
+              flex: "none",
+            }}
+          >
+            <SEmptyTableCellContent styleOverwrite={{ paddingLeft: "20px" }}>
+              <SEmptyTableSkeleton width="48px" height="48px" />
+            </SEmptyTableCellContent>
+          </SEmptyTableCell>
+          <SEmptyTableCell styleOverwrite={{ flex: "1 1 134px" }}>
+            <SEmptyTableCellContent
+              styleOverwrite={{
+                paddingLeft: "0",
+                fontWeight: 600,
+              }}
+            >
+              <SEmptyTableSkeleton width="64px" height="20px" />
+            </SEmptyTableCellContent>
+          </SEmptyTableCell>
+          <SEmptyTableCell styleOverwrite={{ flex: "1 1 114px" }}>
+            <SEmptyTableCellContent>
+              <SEmptyTableSkeleton width="64px" height="20px" />
+            </SEmptyTableCellContent>
+          </SEmptyTableCell>
+          <SEmptyTableCell styleOverwrite={{ flex: "1 1 153px" }}>
+            <SEmptyTableCellContent>
+              <SEmptyTableSkeleton width="64px" height="20px" />
+            </SEmptyTableCellContent>
+          </SEmptyTableCell>
+          <SEmptyTableCell styleOverwrite={{ flex: "0 1 110px" }}>
+            <SEmptyTableCellContent>
+              <SEmptyTableSkeleton width="59px" height="24px" />
+            </SEmptyTableCellContent>
+          </SEmptyTableCell>
+          <SEmptyTableCell styleOverwrite={{ flex: "0 0 134px" }}>
+            <SEmptyTableCellContent />
+          </SEmptyTableCell>
+        </SEmptyTableRow>
+      ))}
+      <SEmptyTableBanner>
+        <EmptyBannerImage />
+        <SEmptyTableBannerTitle>{title}</SEmptyTableBannerTitle>
+        <SEmptyTableBannerDescription>{description}</SEmptyTableBannerDescription>
+      </SEmptyTableBanner>
+    </SEmptyTable>
   );
 };
