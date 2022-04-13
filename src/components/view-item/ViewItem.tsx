6a718e236b4392f6a9426a234d982a1f0a49a144
Youssef Bentaleb <ybentaleb@octo.com>
2022-04-13 00:53:36 +0000

    handle hover viewsitem

--- src/components/view-item/ViewItem.tsx
+++ src/components/view-item/ViewItem.tsx
@@ -5,11 +5,12 @@ interface IViewItem {
   children?;
   isChild?: boolean;
   active?: boolean;
+  hover?: boolean;
 }
 
-export const ViewItem: React.FC<IViewItem> = ({ children, isChild, active }) => {
+export const ViewItem: React.FC<IViewItem> = ({ children, isChild, active, hover }) => {
   return (
-    <SViewItem isChild={isChild} active={active}>
+    <SViewItem isChild={isChild} active={active} hover={hover}>
       {children}
     </SViewItem>
   );
