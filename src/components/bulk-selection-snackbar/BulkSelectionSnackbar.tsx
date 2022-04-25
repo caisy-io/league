eb6dc8067414c26a46ae9352ce988358c41115b5
MAHMOUD <mahmoud.taha.dev@gmail.com>
2022-04-25 15:29:14 +0200

    add props to BulkSelectionSnackbar

--- src/components/bulk-selection-snackbar/BulkSelectionSnackbar.tsx
+++ src/components/bulk-selection-snackbar/BulkSelectionSnackbar.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import { IconClose } from "../../icons/IconClose";
+import React, { FC, ReactNode } from "react";
+import { IconClose } from "../../icons";
 import { SBulkSelectionSnackbarErrorsBadge } from "./styles/SBulkSelectionSnackbarErrorsBadge";
 import { SBulkSelectionSnackbar } from "./styles/SBulkSelectionSnackbar";
 import { SBulkSelectionSnackbarChildren } from "./styles/SBulkSelectionSnackbarChildren";
@@ -13,20 +13,33 @@ interface IBulkSelectionSnackbar {
   selectedItems: number;
   errors: number;
   onClose: () => void;
+  itemSelectedText?: string | ReactNode;
+  itemsSelectedText?: string | ReactNode;
+  errorOccurredText?: string | ReactNode;
+  errorsOccurredText?: string | ReactNode;
 }
 
-export const BulkSelectionSnackbar: FC<IBulkSelectionSnackbar> = ({ selectedItems, errors, onClose, children }) => {
+export const BulkSelectionSnackbar: FC<IBulkSelectionSnackbar> = ({
+  selectedItems,
+  errors,
+  onClose,
+  children,
+  itemSelectedText,
+  itemsSelectedText,
+  errorOccurredText,
+  errorsOccurredText,
+}) => {
   return (
     <SBulkSelectionSnackbarWrapper>
       <SBulkSelectionSnackbar>
         <SBulkSelectionSnackbarSelected>
           <SBulkSelectionSnackbarSelectedBadge>{selectedItems}</SBulkSelectionSnackbarSelectedBadge>
-          {`item${selectedItems !== 1 ? "s" : ""} selected`}
+          {selectedItems !== 1 ? itemsSelectedText ?? "items selected" : itemSelectedText ?? "item selected"}
         </SBulkSelectionSnackbarSelected>
         {errors > 0 && (
           <SBulkSelectionSnackbarErrors>
             <SBulkSelectionSnackbarErrorsBadge>{errors}</SBulkSelectionSnackbarErrorsBadge>
-            {`error${errors !== 1 ? "s" : ""} occurred`}
+            {errors !== 1 ? errorsOccurredText ?? "errors occurred" : errorOccurredText ?? "error occurred"}
           </SBulkSelectionSnackbarErrors>
         )}
         <SBulkSelectionSnackbarChildren>{children}</SBulkSelectionSnackbarChildren>
