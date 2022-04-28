7c7b7fdd63e3327dfda4e34db922b74499368889
mahmoudtahadev <97168820+mahmoudtahadev@users.noreply.github.com>
2022-04-28 09:52:42 +0200

    Bug date picker is broke cy 1335 (#385)
    * fix bug
    
    * fix bug
    
    Co-authored-by: MAHMOUD <mahmoud.taha.dev@gmail.com>

--- src/components/datepicker/DatePicker.tsx
+++ src/components/datepicker/DatePicker.tsx
@@ -177,17 +177,17 @@ const WrappedDatePicker: React.FC<IDatePicker> = ({
 
   React.useEffect(() => {
     if (value) {
-      console.log(`React.useEffect value`, {value});
+      console.log(`React.useEffect value`, { value });
       setDate(value);
-      if(value[0]){
-        setHours(
-          +value[0].getHours() >= 12
-            ? ((+value[0].getHours() - 12) as THourOptions)
-            : (+value[0].getHours() as THourOptions),
-        );
-        setMinutes((Math.round(+value[0].getMinutes() / 5) * 5) as TMinutesOptions);
-        setIsAm(+value[0].getHours() < 12);
-  
+
+      if (value[0]) {
+        const newDate = new Date(value[0]);
+        const newDateHours = newDate.getHours();
+
+        setHours(+newDateHours >= 12 ? ((+newDateHours - 12) as THourOptions) : (+newDateHours as THourOptions));
+        setMinutes((Math.round(+newDate.getMinutes() / 5) * 5) as TMinutesOptions);
+        setIsAm(+newDateHours < 12);
+
         if (value?.filter((el) => el).length) {
           setCalendarMonth(dayjs(value[0]).format("MMMM"));
           setCalendarYear(dayjs(value[0]).format("YYYY"));
@@ -201,18 +201,18 @@ const WrappedDatePicker: React.FC<IDatePicker> = ({
 
   const getCurrentTime = () => {
     const newDate = new Date();
-    setHours(
-      +newDate.getHours() >= 12 ? ((+newDate.getHours() - 12) as THourOptions) : (+newDate.getHours() as THourOptions),
-    );
+    const newDateHours = newDate.getHours();
+    setHours(+newDateHours >= 12 ? ((+newDateHours - 12) as THourOptions) : (+newDateHours as THourOptions));
     const existDate = date[0];
-    existDate.setHours(newDate.getHours());
+    existDate.setHours(newDateHours);
     setMinutes((Math.round(+newDate.getMinutes() / 5) * 5) as TMinutesOptions);
     existDate.setMinutes(newDate.getMinutes());
-    setIsAm(+newDate.getHours() < 12);
+    setIsAm(+newDateHours < 12);
 
     setDate([existDate]);
     onChange?.([existDate]);
   };
+
   const reference = React.useRef(null);
   const flatRef = React.useRef<Flatpickr>();
   const monthRefContainer = React.useRef<HTMLDivElement>(null);
