e328869c23c081401414f8b3b7a06404b0ea014f
Youssef Bentaleb <ybentaleb@octo.com>
2022-04-30 16:37:01 +0000

    add-styleoverwrite-float-input

--- src/components/floating-search-input/FloatingSearchInput.tsx
+++ src/components/floating-search-input/FloatingSearchInput.tsx
@@ -1,4 +1,4 @@
-import React, {  forwardRef, MouseEventHandler,  useState } from "react";
+import React, { forwardRef, MouseEventHandler, useState } from "react";
 import { SFloatingSearchInput } from "./styles/SFloatingSearchInput";
 import { SFloatingSearchInputWrapper } from "./styles/SFLoatingSearchInputWrapper";
 
@@ -7,34 +7,37 @@ interface IFloatingSearchInput {
   value?: string;
   onChange?: (e) => void;
   onClick?: (e: MouseEventHandler<HTMLDivElement>) => void;
+  styleOverwrite?: string;
 }
 
-export const FloatingSearchInput = forwardRef<HTMLInputElement, IFloatingSearchInput>(({ value, onClick, placeholder, onChange }, inputRef) => {
-  const [active, setActive] = useState(false);
+export const FloatingSearchInput = forwardRef<HTMLInputElement, IFloatingSearchInput>(
+  ({ value, onClick, placeholder, onChange, styleOverwrite }, inputRef) => {
+    const [active, setActive] = useState(false);
 
-  const handleClick = (e) => {
-    if(inputRef && (inputRef as any ).current){
-      (inputRef as any ).current.focus();
-    }
-    if(onClick){
-      onClick(e)
-    }
-  };
+    const handleClick = (e) => {
+      if (inputRef && (inputRef as any).current) {
+        (inputRef as any).current.focus();
+      }
+      if (onClick) {
+        onClick(e);
+      }
+    };
 
-  const handleFocus = () => {
-    setActive(true);
-  };
+    const handleFocus = () => {
+      setActive(true);
+    };
 
-  return (
-    <SFloatingSearchInputWrapper active={active} onClick={handleClick}>
-      <SFloatingSearchInput
-        ref={inputRef}
-        type="text"
-        value={value}
-        onFocus={handleFocus}
-        onChange={onChange}
-        placeholder={placeholder}
-      />
-    </SFloatingSearchInputWrapper>
-  );
-});
+    return (
+      <SFloatingSearchInputWrapper active={active} onClick={handleClick} styleOverwrite={styleOverwrite}>
+        <SFloatingSearchInput
+          ref={inputRef}
+          type="text"
+          value={value}
+          onFocus={handleFocus}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
+      </SFloatingSearchInputWrapper>
+    );
+  },
+);
