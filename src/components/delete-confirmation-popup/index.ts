ce1e12ebae165ff15e4d7560ada36fd84b85406b
Nicolas Shiken <nicolasshiken@gmail.com>
2022-07-04 15:12:36 -0300

    bug-wrong-popover-design-cy-1541 (#418)
    * added delete confirmation popup component
    
    * Update index.ts
    
    * added content component

---
+++ src/components/delete-confirmation-popup/index.ts
@@ -0,0 +1,4 @@
+export { DeleteConfirmationPopup } from "./DeleteConfirmationPopup";
+export { DeleteConfirmationPopupContent } from "./DeleteConfirmationPopupContent";
+export { DeleteConfirmationPopupTitle } from "./DeleteConfirmationPopupTitle";
+export { DeleteConfirmationPopupDescription } from "./DeleteConfirmationPopupDescription";
---
+++ src/components/delete-confirmation-popup/styles/SDeleteConfirmationPopupTitle.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { CSSProgressiveBody03Semibold } from "../../../constants/styles";
+
+export const SDeleteConfirmationPopupTitle = styled.div`
+  ${CSSProgressiveBody03Semibold};
+  color: var(--text-01);
+`;
