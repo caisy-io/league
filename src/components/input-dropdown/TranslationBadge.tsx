8571ad4a011a52813020484b1e66a02a5cd4583f
Moritz Gruber <m.gruber@cyclus.digital>
2022-07-21 16:18:58 +0200

    chore: remove flag from TranslationBadge
    chore: remove flag from TranslationBadge

--- src/components/input-dropdown/TranslationBadge.tsx
+++ src/components/input-dropdown/TranslationBadge.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { Flag } from '../flag';
 import { STranslationBadge } from './styles/STranslationBadge';
 
-export interface ITranslationBadgeProps {
-  countryCode: any;
-}
-
-export const TranslationBadge: React.FC<ITranslationBadgeProps> = ({countryCode }) => {
+export const TranslationBadge: React.FC = () => {
   return (
     <STranslationBadge>
-      <Flag countryCode={countryCode} size={16} />
       DE
     </STranslationBadge>
   )
--- src/components/input-fields/TranslationBadge.tsx
+++ src/components/input-fields/TranslationBadge.tsx
@@ -1,10 +1,8 @@
 import { FC } from "react";
-import { Flag } from "../flag/Flag";
 import { STranslationBadge } from "./styles/STranslationBadge";
 
 export const TranslationBadge: FC<{ countryCode: string }> = ({ countryCode }) => (
   <STranslationBadge>
-    <Flag countryCode={countryCode?.toLocaleLowerCase()} size={16} />
     {countryCode?.toUpperCase()}
   </STranslationBadge>
 );
