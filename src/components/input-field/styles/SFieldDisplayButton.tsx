07076566e8c5c68467ce53af29c65744655c7b87
Moritz Gruber <m.gruber@cyclus.digital>
2022-10-07 10:57:50 +0200

    fix: some css in the doucment unfold
    fix: some css in the doucment unfold with long description 🐛

--- src/components/input-field/styles/SFieldDisplayButton.tsx
+++ src/components/input-field/styles/SFieldDisplayButton.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 export const SFieldDisplayButton = styled.div<{ required: boolean; isOpen: boolean }>`
+  align-self: baseline;
+
   svg {
     transition: transform 0.2s ease-in-out;
 
