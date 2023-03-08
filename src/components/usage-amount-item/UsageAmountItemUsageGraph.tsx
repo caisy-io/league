27af9ee99b3b4b05861c1b19ee3efbf732f59463
Nicolas Shiken <nicolasshiken@gmail.com>
2023-03-08 13:50:11 -0300

    Update UsageAmountItemUsageGraph.tsx

--- src/components/usage-amount-item/UsageAmountItemUsageGraph.tsx
+++ src/components/usage-amount-item/UsageAmountItemUsageGraph.tsx
@@ -3,31 +3,33 @@ import { SUsageAmountItemUsageGraph } from "./styles/SUsageAmountItemUsageGraph"
 
 interface IHomePageStatisticGraph {
   percentage: number;
+  size?: number;
+  barColor?: string;
+  barWidth?: string;
 }
 
-const UsageAmountItemUsageGraph: FC<IHomePageStatisticGraph> = ({ percentage }) => {
+const UsageAmountItemUsageGraph: FC<IHomePageStatisticGraph> = ({ percentage, size, barColor, barWidth }) => {
   return (
     <SUsageAmountItemUsageGraph>
-      <svg style={{}} viewBox="0 0 36 36">
+      <svg style={{}} viewBox={`0 0 ${size || "36"} ${size || "36"}`}>
         <path
           d="M18 2.0845
 a 15.9155 15.9155 0 0 1 0 31.831
 a 15.9155 15.9155 0 0 1 0 -31.831"
           fill="none"
-          stroke="var(--ui-03)"
-          strokeWidth="3"
+          stroke={`${barColor || "var(--ui-03)"}`}
+          strokeWidth={`${barWidth || "3"}`}
           strokeDasharray="100"
         />
       </svg>
-      <svg viewBox="0 0 36 36">
+      <svg viewBox={`0 0 ${size || "36"} ${size || "36"}`}>
         <path
           d="M18 2.0845
 a 15.9155 15.9155 0 0 1 0 31.831
 a 15.9155 15.9155 0 0 1 0 -31.831"
           fill="none"
-          strokeWidth="3"
+          strokeWidth={`${barWidth || "3"}`}
           strokeDasharray={`${percentage}, 100`}
-          // strokeDashoffset={100}
         />
       </svg>
     </SUsageAmountItemUsageGraph>
