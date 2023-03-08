e6036fe24296feca26e7cc66073293554a1222e4
Nicolas Shiken <nicolasshiken@gmail.com>
2023-03-08 13:59:46 -0300

    Revert "fix: size and color props"
    This reverts commit 9cf19d2dd42f218a51035b4cea73bb7561ce1416.

--- src/components/usage-amount-item/UsageAmountItemUsageGraph.tsx
+++ src/components/usage-amount-item/UsageAmountItemUsageGraph.tsx
@@ -10,14 +10,14 @@ interface IHomePageStatisticGraph {
 
 const UsageAmountItemUsageGraph: FC<IHomePageStatisticGraph> = ({ percentage, size, barColor, barWidth }) => {
   return (
-    <SUsageAmountItemUsageGraph barColor={barColor} size={size}>
+    <SUsageAmountItemUsageGraph>
       <svg style={{}} viewBox={`0 0 ${size || "36"} ${size || "36"}`}>
         <path
           d="M18 2.0845
 a 15.9155 15.9155 0 0 1 0 31.831
 a 15.9155 15.9155 0 0 1 0 -31.831"
           fill="none"
-          stroke="var(--ui-03)"
+          stroke={`${barColor || "var(--ui-03)"}`}
           strokeWidth={`${barWidth || "3"}`}
           strokeDasharray="100"
         />
--- src/components/usage-amount-item/styles/SUsageAmountItemUsageGraph.ts
+++ src/components/usage-amount-item/styles/SUsageAmountItemUsageGraph.ts
@@ -2,16 +2,16 @@ import styled from "styled-components";
 
 export const SUsageAmountItemUsageGraph = styled.div`
   position: relative;
-  height: ${({ size }) => (size ? size : "32px")};
-  width: ${({ size }) => (size ? size : "32px")};
+  height: 32px;
+  width: 32px;
   display: flex;
   align-items: center;
   justify-content: center;
 
   svg {
     position: absolute;
-    height: ${({ size }) => (size ? size : "32px")};
-    width: ${({ size }) => (size ? size : "32px")};
-    stroke: ${({ barColor }) => (barColor ? barColor : "var(--ui-supportive-06)")};
+    height: 32px;
+    width: 32px;
+    stroke: var(--ui-supportive-06);
   }
 `;
