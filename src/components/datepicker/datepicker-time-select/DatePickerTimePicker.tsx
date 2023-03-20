6a0a16eb74f364eac1be59b91ccaf90f26552b73
bfloku <abso.floku@gmail.com>
2023-03-20 13:55:13 -0300

    Update DatePickerTimePicker.tsx

--- src/components/datepicker/datepicker-time-select/DatePickerTimePicker.tsx
+++ src/components/datepicker/datepicker-time-select/DatePickerTimePicker.tsx
@@ -9,12 +9,12 @@ interface IDatePickerTimePicker {
   value: string;
   options: string[];
   show: boolean;
-  onClickOutside: () => void
+  onClickOutside: () => void;
 }
 
-const DatePickerTimePicker: React.FC<IDatePickerTimePicker> = ({...props}) => {
-  const {setHours, setMinutes, setShowHours, setShowMinutes, onChange, date, hours, minutes} = usePicker();
-  
+const DatePickerTimePicker: React.FC<IDatePickerTimePicker> = ({ ...props }) => {
+  const { setHours, setMinutes, setShowHours, setShowMinutes, onChange, date, hours, minutes } = usePicker();
+
   const setTime = (e, value) => {
     e.stopPropagation();
     switch (props.value) {
@@ -22,34 +22,28 @@ const DatePickerTimePicker: React.FC<IDatePickerTimePicker> = ({...props}) => {
         setMinutes(value);
         setShowHours(false);
         setShowMinutes(false);
-        date && onChange([new Date(
-          date[0].getFullYear(),
-          date[0].getMonth(),
-          date[0].getDate(),
-          hours,
-          value,
-        )]);
+        date && onChange([new Date(date[0].getFullYear(), date[0].getMonth(), date[0].getDate(), hours, value)]);
         return;
       case "hours":
         setHours(value);
         setShowHours(false);
         setShowMinutes(false);
-        date && onChange([new Date(
-          date[0].getFullYear(),
-          date[0].getMonth(),
-          date[0].getDate(),
-          value,
-          minutes,
-        )]);
+        date && onChange([new Date(date[0].getFullYear(), date[0].getMonth(), date[0].getDate(), value, minutes)]);
         return;
     }
   };
-  
+
   return (
     <>
       {props.show && (
         <ClickOutside onClickOutside={props.onClickOutside}>
-          <SDatePickerTimePicker>
+          <SDatePickerTimePicker
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              props.onClickOutside();
+            }}
+          >
             <DatePickerCard>
               {props.options.map((option) => (
                 <SDatePickerTimePickerOption onClick={(e) => setTime(e, option)} key={option}>
