ba7cf3e545d64f9d70897da692223027c51edca8
bfloku <abso.floku@gmail.com>
2023-03-29 10:59:40 -0300

    Update SActiveUserAvatarBadge.ts

--- src/components/active-user-avatar/styles/SActiveUserAvatarBadge.ts
+++ src/components/active-user-avatar/styles/SActiveUserAvatarBadge.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const SActiveUserAvatarBadge = styled.div`
-  height: 8px;
-  width: 8px;
+  height: 10px;
+  width: 10px;
   border: 2px solid var(--ui-01);
   background-color: ${({ color }) => color};
   box-sizing: border-box;
