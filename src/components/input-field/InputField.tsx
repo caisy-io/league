6c39ae82dbca22ffe68d922926029c8861d1923b
bfloku <abso.floku@gmail.com>
2023-04-04 08:46:31 -0300

    formatting

--- src/components/input-field/InputField.tsx
+++ src/components/input-field/InputField.tsx
@@ -1,28 +1,28 @@
-import {FC, ReactNode, useCallback, useState} from "react";
-import {Tooltip} from "../tooltip";
-import {IconQuestionCircle, IconChevronDown} from "../../icons";
-import {SFieldInputWrapper} from "./styles/SFieldInputWrapper";
-import {SFieldItem} from "./styles/SFieldItem";
-import {SFieldHeader} from "./styles/SFieldHeader";
-import {IconButton} from "../icon-button";
-import {SFieldTitle} from "./styles/SFieldTitle";
-import {SFieldDisplayButton} from "./styles/SFieldDisplayButton";
-import {SFieldDescription} from "./styles/SFieldDescription";
-import {SFieldErrors} from "./styles/SFieldErrors";
-import {IInputFieldProps} from "./types";
+import { FC, ReactNode, useCallback, useState } from "react";
+import { Tooltip } from "../tooltip";
+import { IconQuestionCircle, IconChevronDown } from "../../icons";
+import { SFieldInputWrapper } from "./styles/SFieldInputWrapper";
+import { SFieldItem } from "./styles/SFieldItem";
+import { SFieldHeader } from "./styles/SFieldHeader";
+import { IconButton } from "../icon-button";
+import { SFieldTitle } from "./styles/SFieldTitle";
+import { SFieldDisplayButton } from "./styles/SFieldDisplayButton";
+import { SFieldDescription } from "./styles/SFieldDescription";
+import { SFieldErrors } from "./styles/SFieldErrors";
+import { IInputFieldProps } from "./types";
 
 export const InputField: FC<IInputFieldProps> = ({
-                                                   title,
-                                                   description,
-                                                   icon,
-                                                   required,
-                                                   tooltip,
-                                                   errors,
-                                                   children,
-                                                   onExpand,
-                                                   onCollapse,
-  id
-                                                 }) => {
+  title,
+  description,
+  icon,
+  required,
+  tooltip,
+  errors,
+  children,
+  onExpand,
+  onCollapse,
+  id,
+}) => {
   const [isOpen, setOpen] = useState(true);
   const toggleOpen = useCallback(() => {
     if (isOpen) {
@@ -32,18 +32,18 @@ export const InputField: FC<IInputFieldProps> = ({
     }
     setOpen(!isOpen);
   }, [isOpen, setOpen]);
-  
+
   return (
     <SFieldItem id={id} error={!!errors}>
       <SFieldHeader isOpen={isOpen}>
         {icon}
-        <div style={{flex: '1 1 auto'}}>
+        <div style={{ flex: "1 1 auto" }}>
           <SFieldTitle required={required}>
             <h3>{title}</h3>
             {tooltip && (
               <div data-tooltip-icon>
                 <Tooltip content={tooltip} placement="right" color="black">
-                  <IconQuestionCircle size={16}/>
+                  <IconQuestionCircle size={16} />
                 </Tooltip>
               </div>
             )}
@@ -52,12 +52,12 @@ export const InputField: FC<IInputFieldProps> = ({
         </div>
         <SFieldDisplayButton data-display-button isOpen={isOpen}>
           <IconButton type="secondary" onClick={toggleOpen}>
-            <IconChevronDown/>
+            <IconChevronDown />
           </IconButton>
         </SFieldDisplayButton>
       </SFieldHeader>
       <SFieldInputWrapper isOpen={isOpen}>{children}</SFieldInputWrapper>
-      
+
       {errors && (
         <SFieldErrors>
           {errors.map((error: ReactNode) => (
