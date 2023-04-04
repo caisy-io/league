b4b78056e163009bf6fe6b5384797693ee93796c
bfloku <abso.floku@gmail.com>
2023-04-04 09:43:13 -0300

    fix: build

--- src/components/string-single-field/StringSingleField.tsx
+++ src/components/string-single-field/StringSingleField.tsx
@@ -1,9 +1,24 @@
 import { FC } from "react";
 import { InputField, IInputFieldProps } from "../input-field";
 
-export const StringSingleField: FC<IInputFieldProps> = ({ title, description, required, tooltip, errors, children, usersList }) => {
+export const StringSingleField: FC<IInputFieldProps> = ({
+  title,
+  description,
+  required,
+  tooltip,
+  errors,
+  children,
+  usersListComponent,
+}) => {
   return (
-    <InputField title={title} description={description} required={required} tooltip={tooltip} errors={errors} usersList={usersList}>
+    <InputField
+      title={title}
+      description={description}
+      required={required}
+      tooltip={tooltip}
+      errors={errors}
+      usersListComponent={usersListComponent}
+    >
       {children}
     </InputField>
   );
