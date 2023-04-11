1a1d93be8e683f92cda027e802cf21b02756d579
bfloku <abso.floku@gmail.com>
2023-04-11 11:13:04 -0300

    fix: styling

--- src/components/sidebar-card/styles/SSidebarCardSubtitle.tsx
+++ src/components/sidebar-card/styles/SSidebarCardSubtitle.tsx
@@ -1,12 +1,11 @@
-import styled, { css } from 'styled-components';
-import { CSSProgressiveCaption02Medium } from '../../../constants/styles/design-tokens/fonts/CSSTypographies';
-import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from '../../../constants/styles/mediaquerys';
+import styled, { css } from "styled-components";
+import { CSSProgressiveCaption02Medium } from "../../../constants/styles/design-tokens/fonts/CSSTypographies";
+import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles/mediaquerys";
 
 const Bronze = css`
   ${CSSProgressiveCaption02Medium};
   color: var(--text-04);
   margin-right: 0.25rem;
-  word-break: break-all;
 `;
 
 const Silver = css``;
@@ -20,8 +19,7 @@ const Diamond = css``;
 export const SSidebarCardSubtitle = styled.p`
   ${Bronze}
   ${MIN_SILVER`${Silver}`};
-  ${MIN_GOLD`${Gold}`}; 
+  ${MIN_GOLD`${Gold}`};
   ${MIN_PLATINUM`${Platinum}`};
   ${MIN_DIAMOND`${Diamond}`};
 `;
-
--- src/components/sidebar-card/styles/SSidebarCardTitle.tsx
+++ src/components/sidebar-card/styles/SSidebarCardTitle.tsx
@@ -1,13 +1,12 @@
-import styled, { css } from 'styled-components';
-import { CSSProgressiveCaption01Semibold } from '../../../constants/styles/design-tokens/fonts/CSSTypographies';
-import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from '../../../constants/styles/mediaquerys';
+import styled, { css } from "styled-components";
+import { CSSProgressiveCaption01Semibold } from "../../../constants/styles/design-tokens/fonts/CSSTypographies";
+import { MIN_SILVER, MIN_GOLD, MIN_PLATINUM, MIN_DIAMOND } from "../../../constants/styles/mediaquerys";
 
 const Bronze = css`
   ${CSSProgressiveCaption01Semibold};
   text-overflow: ellipsis;
   overflow: hidden;
   white-space: nowrap;
-  max-width: 170px;
   margin-bottom: 0.25rem;
 `;
 
@@ -22,8 +21,7 @@ const Diamond = css``;
 export const SSidebarCardTitle = styled.h3`
   ${Bronze}
   ${MIN_SILVER`${Silver}`};
-  ${MIN_GOLD`${Gold}`}; 
+  ${MIN_GOLD`${Gold}`};
   ${MIN_PLATINUM`${Platinum}`};
   ${MIN_DIAMOND`${Diamond}`};
 `;
-
