0b29bbc792f4b78e32b0874095505a00e79fb34d
bfloku <127332419+bfloku@users.noreply.github.com>
2023-05-23 13:05:47 -0300

    improvement-autosave-feedback-cy-2379 (#477)
    * chore: added saving text to blockeditorsidebar
    
    * Update BlockEditorSidebar.tsx

---
+++ src/components/block-editor-sidebar/styles/SBlockEditorSidebarStatusIndicatorSaving.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { CSSProgressiveCaption02 } from "../../../constants/styles";
+
+export const SBlockEditorSidebarStatusIndicatorSaving = styled.div`
+  ${CSSProgressiveCaption02};
+  color: var(--text-03);
+`;
