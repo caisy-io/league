bb18ced10b956b209425d94dc898fab2de2830df
bfloku <abso.floku@gmail.com>
2023-05-30 14:24:20 -0300

    Update SMultiselectInputDropdownSelectList.ts

--- src/components/multiselect-input-dropdown/styles/SMultiselectInputDropdownSelectList.ts
+++ src/components/multiselect-input-dropdown/styles/SMultiselectInputDropdownSelectList.ts
@@ -4,5 +4,5 @@ import { CSSScrollbarStyles } from "../../../constants/styles";
 export const SMultiselectInputDropdownSelectList = styled.div`
   overflow: auto;
   ${CSSScrollbarStyles};
-  max-height: ${({ maxHeight }) => `${maxHeight}px`};
+  max-height: ${({ maxHeight }) => (maxHeight ? `${maxHeight}px` : "initial")};
 `;
