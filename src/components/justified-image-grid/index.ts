36d346350bc01597d8652002302272c67a97ed59
Moritz Gruber <m.gruber@cyclus.digital>
2023-06-09 07:57:45 +0200

    chore: expose more
    chore: expose more

--- src/components/justified-image-grid/index.ts
+++ src/components/justified-image-grid/index.ts
@@ -2,5 +2,7 @@ export { JustifiedImageGrid } from "./JustifiedImageGrid";
 export { dynamicConfig } from "./dynamicConfig";
 export { getDefaultConfig } from "./defaultConfig";
 export { decodeStateSnapshotURLSafe, encodeStateSnapshotURLSafe } from "./stateHelper";
+export { stretchImages } from "./generateRows";
+export { generateRows } from "./generateRows";
+export { LazyImage } from "./LazyImage";
 export * from "./types";
-export {stretchImages} from "./generateRows";
