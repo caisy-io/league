531463a64b0973a7aa32a4600ed66f00b6401855
Hassan Mostafa <131889080+HassanMoe@users.noreply.github.com>
2023-07-23 15:23:31 +0300

    exporting focal point feature icons (#501)

---
+++ src/icons/setFocalPointHeaderIcon.tsx
@@ -0,0 +1,45 @@
+export const SetFocalPointHeaderIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M16 21H8C5.239 21 3 18.761 3 16V8C3 5.239 5.239 3 8 3H16C18.761 3 21 5.239 21 8V16C21 18.761 18.761 21 16 21Z"
+      stroke="#172C55"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M14.989 10.511C13.878 10.511 12.978 9.61001 12.978 8.50001V8.50001C12.978 7.38901 13.879 6.48901 14.989 6.48901"
+      stroke="#172C55"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8.96608 6.48901H14.9891C16.0991 6.48901 17.0001 7.38901 17.0001 8.50001V8.50001C17.0001 9.61101 16.0991 10.511 14.9891 10.511H8.96608C7.85508 10.511 6.95508 9.61001 6.95508 8.50001V8.50001C6.95508 7.38901 7.85608 6.48901 8.96608 6.48901Z"
+      stroke="#172C55"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.96582 13.489C10.0768 13.489 10.9768 14.39 10.9768 15.5V15.5C10.9768 16.611 10.0758 17.511 8.96582 17.511"
+      stroke="#172C55"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M14.9891 17.511H8.96608C7.85508 17.511 6.95508 16.61 6.95508 15.5V15.5C6.95508 14.389 7.85608 13.489 8.96608 13.489H14.9891C16.1001 13.489 17.0001 14.39 17.0001 15.5V15.5C17.0001 16.611 16.0991 17.511 14.9891 17.511Z"
+      stroke="#172C55"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
---
+++ src/icons/setFocalPointIcon.tsx
@@ -0,0 +1,36 @@
+export const SetFocalPoint = () => {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M11.3 4.70021C13.1224 6.52266 13.1224 9.47742 11.3 11.2999C9.47754 13.1223 6.52278 13.1223 4.70033 11.2999C2.87788 9.47742 2.87788 6.52266 4.70033 4.70021C6.52278 2.87776 9.47754 2.87776 11.3 4.70021"
+        stroke="#172C55"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.94265 7.05715C9.46335 7.57785 9.46335 8.42207 8.94265 8.94277C8.42195 9.46347 7.57773 9.46347 7.05703 8.94277C6.53633 8.42207 6.53633 7.57785 7.05703 7.05715C7.57773 6.53645 8.42195 6.53645 8.94265 7.05715"
+        stroke="#172C55"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.99984 14V11.3334"
+        stroke="#172C55"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M7.99984 2V4.66667" stroke="#172C55" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M4.66667 7.99996H2" stroke="#172C55" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M14.0002 7.99996H11.3335"
+        stroke="#172C55"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
