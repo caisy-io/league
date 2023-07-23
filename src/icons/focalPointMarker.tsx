62582008dc5f8d53c545c5fe18ae05f300447888
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-07-23 15:18:07 +0300

    exporting focal point feature icons

---
+++ src/icons/focalPointMarker.tsx
@@ -0,0 +1,86 @@
+export const FocalPointMarker: React.FC = () => (
+  <svg width="185" height="185" viewBox="0 0 185 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g opacity="0.25" filter="url(#filter0_d_22628_7314)">
+      <circle cx="102" cy="90" r="78" fill="url(#paint0_radial_22628_7314)" />
+      <circle cx="102" cy="90" r="77.5" stroke="white" />
+    </g>
+    <g filter="url(#filter1_d_22628_7314)" className="el-g">
+      <circle cx="102" cy="90" r="18" fill="white" />
+    </g>
+    <path
+      d="M106.125 85.8752C108.403 88.1533 108.403 91.8467 106.125 94.1248C103.847 96.4029 100.153 96.4029 97.875 94.1248C95.597 91.8467 95.597 88.1533 97.875 85.8752C100.153 83.5972 103.847 83.5972 106.125 85.8752"
+      stroke="#829DB4"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M103.179 88.8215C103.83 89.4724 103.83 90.5277 103.179 91.1786C102.528 91.8294 101.473 91.8294 100.822 91.1786C100.171 90.5277 100.171 89.4724 100.822 88.8215C101.473 88.1707 102.528 88.1707 103.179 88.8215"
+      stroke="#829DB4"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path d="M102 97.5V94.1667" stroke="#829DB4" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M102 82.5V85.8333" stroke="#829DB4" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M97.8333 90H94.5" stroke="#829DB4" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M109.5 90H106.167" stroke="#829DB4" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+    <defs>
+      <filter
+        id="filter0_d_22628_7314"
+        x="0"
+        y="0"
+        width="185"
+        height="185"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="12" />
+        <feGaussianBlur stdDeviation="12" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_22628_7314" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_22628_7314" result="shape" />
+      </filter>
+      <filter
+        id="filter1_d_22628_7314"
+        x="60"
+        y="60"
+        width="84"
+        height="84"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="12" />
+        <feGaussianBlur stdDeviation="12" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_22628_7314" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_22628_7314" result="shape" />
+      </filter>
+      <radialGradient
+        id="paint0_radial_22628_7314"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(102 90) rotate(90) scale(78)"
+      >
+        <stop offset="0.720702" />
+        <stop offset="1" stopOpacity="0" />
+      </radialGradient>
+    </defs>
+  </svg>
+);
---
+++ src/icons/setFocalPointHeaderCloseIcon.tsx
@@ -0,0 +1,18 @@
+export const SetFocalPointHeaderCloseIcon = () => (
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M10.667 10.6666L21.3337 21.3333"
+      stroke="#829DB4"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M21.3337 10.6666L10.667 21.3333"
+      stroke="#829DB4"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
