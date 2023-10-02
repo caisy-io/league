55f957b9536e9b3f43e18039d4ade814880484fa
MAHMOUD <mahmoud.taha.dev@gmail.com>
2023-10-02 19:06:41 +0300

    change padding

--- src/components/input-field/styles/SFieldInputWrapper.tsx
+++ src/components/input-field/styles/SFieldInputWrapper.tsx
@@ -8,9 +8,9 @@ export const SFieldInputWrapper = styled.div<{ isOpen: boolean; wrapperBgColor?:
   display: none;
   flex-direction: column;
   align-items: center;
-  ${({isOpen}) => isOpen && CSSExpanded}
   border-top: 1px solid var(--ui-03);
-  padding: 1rem;
-  gap: 0.5rem;
+  padding: 12px;
+  gap: 12px;
   background-color: ${({wrapperBgColor}) => wrapperBgColor || "var(--ui-01)"};
+  ${({isOpen}) => isOpen && CSSExpanded};
 `;
