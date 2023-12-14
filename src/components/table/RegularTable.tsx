15378b60c4c4bfd675c0bb8aae26d57721a83920
bfloku <abso.floku@gmail.com>
2023-12-14 11:58:54 -0300

    feat: made horizontal scroll optional

---
+++ src/components/table/RegularTable.tsx
@@ -0,0 +1,194 @@
+import React, { FC, forwardRef, useEffect, useMemo, useRef, useState } from "react";
+import { STable } from "./styles/STable";
+import { STh } from "./styles/STh";
+import { STr } from "./styles/STr";
+import { STd } from "./styles/STd";
+import { STbody } from "./styles/STbody";
+import { SThead } from "./styles/SThead";
+import { STableLoading } from "./styles/STableLoading";
+import { Empty } from "../empty";
+import debounce from "lodash/debounce";
+import { useDimensions } from "../../utils";
+import { Spinner } from "../spinner";
+import { Virtuoso } from "react-virtuoso";
+import { ITable } from "./Table";
+
+export const RegularTable: FC<ITable> = forwardRef(
+  (
+    {
+      dataSource,
+      isNextPageLoading,
+      loadNextPage = () => null,
+      loading,
+      emptyMessage,
+      style,
+      rowStyle,
+      onRowClick,
+      columns,
+      renderAsFirstRow,
+      empty,
+    },
+    ref,
+  ) => {
+    const containerRef = useRef<any>({});
+    const headerRef = useRef<any>({});
+    const firstRowRef = useRef<HTMLDivElement>(null);
+    const [scrollerRef, setScrollerRef] = useState<HTMLElement | null>(null);
+    const [rowWidth, setRowWidth] = useState<number>();
+    const tableRowsRef = useRef<HTMLDivElement>(null);
+
+    const { height: containerHeight } = useDimensions(containerRef);
+    const { height: headerHeight } = useDimensions(headerRef);
+    const height = containerHeight - headerHeight;
+
+    // Only load 1 page of items at a time.
+    // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+    const loadMoreItems = isNextPageLoading ? () => {} : loadNextPage;
+
+    // Every row is loaded except for our loading indicator row.
+    // const isItemLoaded = (index) => !props.hasNextPage || index < props.dataSource.length;
+    if (!dataSource) {
+      return null;
+    }
+
+    const [initialized, setInitialized] = useState(false);
+
+    useEffect(() => {
+      if (tableRowsRef.current && firstRowRef.current && !initialized) {
+        setInitialized(dataSource.length > 0 && !loading && !isNextPageLoading);
+        tableRowsRef.current.style.transform = `translateY(${
+          firstRowRef.current.offsetHeight > 0 ? firstRowRef.current.offsetHeight : 0
+        }px)`;
+      }
+    }, [tableRowsRef.current, renderAsFirstRow, dataSource, firstRowRef.current]);
+
+    useEffect(() => {
+      if (tableRowsRef.current && firstRowRef.current) {
+        tableRowsRef.current.style.transform = `translateY(${
+          firstRowRef.current.offsetHeight > 0 ? firstRowRef.current.offsetHeight : 0
+        }px)`;
+      }
+    }, [dataSource?.length]);
+
+    useEffect(() => {
+      setRowWidth((scrollerRef as any)?.firstChild?.offsetWidth || "100%");
+    }, [scrollerRef, dataSource?.length, (scrollerRef as any)?.firstChild?.offsetWidth]);
+
+    const RenderRow = ({ index }: { index: number }) => {
+      const row = dataSource[index];
+
+      return (
+        <STr onClick={() => (!!onRowClick ? onRowClick(row) : () => {})} key={`row-${row.id}`}>
+          {columns.map(({ key, renderItem }, keyIndex) => {
+            const header = document.getElementById(`header-${keyIndex}`);
+            const headerWidth = header?.offsetWidth || 0;
+            const isLastCell = keyIndex === columns.length - 1;
+
+            const headerStyle = {
+              width: headerWidth,
+              minWidth: headerWidth,
+            };
+
+            return (
+              <STd
+                key={key}
+                style={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  display: "block",
+                  ...(!isLastCell ? headerStyle : {}),
+                  ...row?.[key]?.style,
+                }}
+              >
+                {renderItem?.(row[key], index) || row[key]}
+              </STd>
+            );
+          })}
+        </STr>
+      );
+    };
+
+    const debouncedLoadMoreItems = useMemo(() => debounce(loadMoreItems, 300), [loadMoreItems]);
+
+    const innerHeaderRef = useRef<HTMLDivElement>(null);
+
+    const TableWithRows = (
+      <Virtuoso
+        components={{
+          Header: () => <div style={{ width: "auto" }}>{renderAsFirstRow}</div>,
+        }}
+        totalCount={dataSource.length}
+        className="league-table"
+        height={height}
+        style={{ minHeight: height }}
+        endReached={debouncedLoadMoreItems}
+        ref={ref as any}
+        scrollerRef={setScrollerRef as any}
+        totalListHeightChanged={(height) => {
+          if (
+            height > (scrollerRef as any)?.offsetHeight &&
+            (scrollerRef as any)?.firstChild?.offsetWidth === rowWidth
+          ) {
+            setRowWidth((scrollerRef as any)?.firstChild?.offsetWidth - 15);
+          } else {
+            setRowWidth((scrollerRef as any)?.firstChild?.offsetWidth);
+          }
+        }}
+        itemContent={(index) => {
+          return <RenderRow index={index} />;
+        }}
+      />
+    );
+
+    return (
+      <STable
+        style={{
+          ...style,
+        }}
+        ref={containerRef}
+      >
+        <SThead
+          style={{
+            height: innerHeaderRef?.current?.offsetHeight,
+          }}
+          ref={headerRef}
+        >
+          <STr ref={innerHeaderRef} style={{ ...rowStyle, position: "absolute", width: "100%" }}>
+            {columns.map((column, columnIndex) => {
+              const id = `header-${columnIndex}`;
+
+              return (
+                <STh
+                  key={columnIndex}
+                  id={id}
+                  style={{
+                    ...column.style,
+                  }}
+                >
+                  {column.header}
+                </STh>
+              );
+            })}
+          </STr>
+        </SThead>
+        <STbody>
+          {loading ? (
+            <STableLoading height={height}>
+              <Spinner />
+            </STableLoading>
+          ) : (
+            <>
+              {dataSource?.length ? (
+                <>{!!renderAsFirstRow ? <div ref={tableRowsRef}>{TableWithRows}</div> : <>{TableWithRows}</>}</>
+              ) : empty ? (
+                empty
+              ) : (
+                <Empty type="blueprint" title="" description={emptyMessage || "No data found."} />
+              )}
+            </>
+          )}
+        </STbody>
+      </STable>
+    );
+  },
+);
--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,16 +1,6 @@
-import React, { CSSProperties, FC, forwardRef, ReactNode, useEffect, useMemo, useRef, useState } from "react";
-import { STable } from "./styles/STable";
-import { STh } from "./styles/STh";
-import { STr } from "./styles/STr";
-import { STd } from "./styles/STd";
-import { STbody } from "./styles/STbody";
-import { SThead } from "./styles/SThead";
-import { STableLoading } from "./styles/STableLoading";
-import { Empty } from "../empty";
-import debounce from "lodash/debounce";
-import { useDimensions } from "../../utils";
-import { Spinner } from "../spinner";
-import { Virtuoso } from "react-virtuoso";
+import React, { CSSProperties, FC, forwardRef, ReactNode } from "react";
+import { HorizontallyScrollableTable } from "./HorizontallyScrollableTable";
+import { RegularTable } from "./RegularTable";
 
 export interface IColumn {
   header: ReactNode;
@@ -25,22 +15,17 @@ interface ITableBase {
   dataSource: any;
   isNextPageLoading?: boolean;
   loadNextPage?: () => Promise<void>;
-  hasNextPage?: boolean;
   loading?: boolean;
   emptyMessage?: string;
   style?: CSSProperties;
   rowStyle?: CSSProperties;
   onRowClick?: (payload: any) => void;
-  onRowHover?: (payload: any) => void;
-  tableOptions?: any;
-  onHeaderClick?: (column: any) => Promise<void>;
   renderAsFirstRow?: JSX.Element;
   ref?: any;
   empty?: ReactNode;
-  tableMinWidth?: number | string;
-  tableMaxHeight?: number | string;
   tableWidth?: number | string;
   containerMaxWidth?: number;
+  isHorizontallyScrollable?: boolean;
 }
 
 interface ITableWithItemSize extends ITableBase {
@@ -53,232 +38,16 @@ interface ITableWithDynamicItemSize extends ITableBase {
   useConditionalItemSize: true;
 }
 
-type ITable = ITableWithItemSize | ITableWithDynamicItemSize;
+export type ITable = ITableWithItemSize | ITableWithDynamicItemSize;
 
-const Scroller = React.forwardRef(({ style, ...props }: any, ref) => {
-  return <div style={{ ...style, width: "auto", minWidth: props.context.containerWidth }} ref={ref} {...props} />;
+export const Table: FC<ITable> = forwardRef(({ isHorizontallyScrollable, ...props }, ref) => {
+  return (
+    <>
+      {isHorizontallyScrollable ? (
+        <HorizontallyScrollableTable ref={ref} {...props} />
+      ) : (
+        <RegularTable ref={ref} {...props} />
+      )}
+    </>
+  );
 });
-
-const ListItem = React.forwardRef(({ style, ...props }: any) => {
-  return <div style={{ ...style, width: "auto", minWidth: props.context.tableWidth }} {...props} />;
-});
-
-export const Table: FC<ITable> = forwardRef(
-  (
-    {
-      dataSource,
-      isNextPageLoading,
-      loadNextPage = () => null,
-      loading,
-      emptyMessage,
-      style,
-      rowStyle,
-      onRowClick,
-      columns,
-      renderAsFirstRow,
-      empty,
-      tableWidth,
-      containerMaxWidth,
-    },
-    ref,
-  ) => {
-    const containerRef = useRef<any>({});
-    const headerRef = useRef<any>({});
-    const [scrollerRef, setScrollerRef] = useState<HTMLElement | null>(null);
-    const [rowWidth, setRowWidth] = useState<number>();
-    const [containerWidth, setContainerWidth] = useState<number | string>("auto");
-
-    const tableRowsRef = useRef<HTMLDivElement>(null);
-
-    const { height: containerHeight } = useDimensions(containerRef);
-    const { height: headerHeight } = useDimensions(headerRef);
-    const height = containerHeight - headerHeight;
-
-    // Only load 1 page of items at a time.
-    // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
-    const loadMoreItems = isNextPageLoading ? () => {} : loadNextPage;
-
-    // Every row is loaded except for our loading indicator row.
-    // const isItemLoaded = (index) => !props.hasNextPage || index < props.dataSource.length;
-    if (!dataSource) {
-      return null;
-    }
-
-    useEffect(() => {
-      setRowWidth((scrollerRef as any)?.firstChild?.offsetWidth || "100%");
-    }, [scrollerRef, dataSource?.length, (scrollerRef as any)?.firstChild?.offsetWidth]);
-
-    const RenderRow = ({ index }: { index: number }) => {
-      const row = dataSource[index];
-
-      return (
-        <STr onClick={() => (!!onRowClick ? onRowClick(row) : () => {})} key={`row-${row.id}`}>
-          {columns.map(({ key, renderItem }, keyIndex) => {
-            const header = document.getElementById(`header-${keyIndex}`);
-            const headerWidth = header?.offsetWidth || 0;
-            const isLastCell = keyIndex === columns.length - 1;
-
-            const headerStyle = {
-              width: headerWidth,
-              minWidth: headerWidth,
-            };
-
-            return (
-              <STd
-                key={key}
-                style={{
-                  textOverflow: "ellipsis",
-                  overflow: "hidden",
-                  display: "block",
-                  ...(!isLastCell ? headerStyle : {}),
-                  ...row?.[key]?.style,
-                }}
-              >
-                {renderItem?.(row[key], index) || row[key]}
-              </STd>
-            );
-          })}
-        </STr>
-      );
-    };
-
-    const debouncedLoadMoreItems = useMemo(() => debounce(loadMoreItems, 300), [loadMoreItems]);
-
-    const innerHeaderRef = useRef<HTMLDivElement>(null);
-
-    const onScroll = (e) => {
-      const scrollLeft = e?.nativeEvent?.target?.scrollLeft;
-
-      (innerHeaderRef.current as HTMLDivElement).style.left = `-${scrollLeft}px`;
-    };
-
-    useEffect(() => {
-      if (typeof window === "undefined") return;
-
-      const handleResize = (e) => {
-        const windowWidth = e.target.innerWidth;
-        const rect = containerRef?.current?.getBoundingClientRect?.();
-        const left = rect?.left;
-        const right = rect?.right;
-
-        const startToWindow = windowWidth - left;
-        const endToWindow = windowWidth - right;
-        const maxWidth = windowWidth;
-
-        const containerWidth = Math.max(startToWindow, endToWindow);
-
-        setContainerWidth(Math.min(containerWidth, maxWidth, containerMaxWidth || 999999));
-      };
-
-      window.addEventListener("resize", handleResize);
-
-      return () => window.removeEventListener("resize", handleResize);
-    }, [typeof window, containerMaxWidth]);
-
-    useEffect(() => {
-      if (!containerMaxWidth) return;
-
-      const windowWidth = window?.innerWidth;
-      const rect = containerRef?.current?.getBoundingClientRect?.();
-      const left = rect?.left;
-      const right = rect?.right;
-
-      const startToWindow = windowWidth - left;
-      const endToWindow = windowWidth - right;
-      const maxWidth = windowWidth;
-
-      const containerWidth = Math.max(startToWindow, endToWindow, containerMaxWidth);
-
-      setContainerWidth(Math.min(containerWidth, maxWidth));
-    }, [containerMaxWidth]);
-
-    const TableWithRows = (
-      <Virtuoso
-        components={{
-          Scroller: Scroller as any,
-          Item: ListItem as any,
-          Header: () => <div style={{ minWidth: tableWidth, width: "auto" }}>{renderAsFirstRow}</div>,
-        }}
-        totalCount={dataSource.length}
-        context={{
-          containerWidth: containerWidth || "auto",
-          tableWidth: tableWidth || "auto",
-        }}
-        onScroll={onScroll}
-        className="league-table"
-        height={height}
-        style={{ minHeight: height }}
-        endReached={debouncedLoadMoreItems}
-        ref={ref as any}
-        scrollerRef={setScrollerRef as any}
-        totalListHeightChanged={(height) => {
-          if (
-            height > (scrollerRef as any)?.offsetHeight &&
-            (scrollerRef as any)?.firstChild?.offsetWidth === rowWidth
-          ) {
-            setRowWidth((scrollerRef as any)?.firstChild?.offsetWidth - 15);
-          } else {
-            setRowWidth((scrollerRef as any)?.firstChild?.offsetWidth);
-          }
-        }}
-        itemContent={(index) => {
-          return <RenderRow index={index} />;
-        }}
-      />
-    );
-
-    return (
-      <STable
-        style={{
-          ...style,
-          maxWidth: containerWidth,
-        }}
-        ref={containerRef}
-      >
-        <SThead
-          style={{
-            width: tableWidth,
-            maxWidth: containerWidth,
-            height: innerHeaderRef?.current?.offsetHeight,
-          }}
-          ref={headerRef}
-        >
-          <STr ref={innerHeaderRef} style={{ ...rowStyle, minWidth: tableWidth, position: "absolute", width: "100%" }}>
-            {columns.map((column, columnIndex) => {
-              const id = `header-${columnIndex}`;
-
-              return (
-                <STh
-                  key={columnIndex}
-                  id={id}
-                  style={{
-                    ...column.style,
-                  }}
-                >
-                  {column.header}
-                </STh>
-              );
-            })}
-          </STr>
-        </SThead>
-        <STbody>
-          {loading ? (
-            <STableLoading height={height}>
-              <Spinner />
-            </STableLoading>
-          ) : (
-            <>
-              {dataSource?.length ? (
-                <>{!!renderAsFirstRow ? <div ref={tableRowsRef}>{TableWithRows}</div> : <>{TableWithRows}</>}</>
-              ) : empty ? (
-                empty
-              ) : (
-                <Empty type="blueprint" title="" description={emptyMessage || "No data found."} />
-              )}
-            </>
-          )}
-        </STbody>
-      </STable>
-    );
-  },
-);
