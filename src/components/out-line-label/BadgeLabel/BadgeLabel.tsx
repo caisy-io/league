4db7e24d707cad74617d80b7669f66b84f8bafdc
mahmoud.taha.dev <mahmoud.taha.dev@gmail.com>
2024-02-20 15:29:34 +0200

    rename children to value

--- src/components/out-line-label/BadgeLabel/BadgeLabel.tsx
+++ src/components/out-line-label/BadgeLabel/BadgeLabel.tsx
@@ -2,12 +2,12 @@ import { FC, ReactNode, CSSProperties } from "react";
 import { SBadgeLabel } from "./SBadgeLabel";
 
 export interface IBadgeLabel {
-  children: ReactNode;
+  value: ReactNode;
   styleOverwrite?: CSSProperties;
 }
 
-const BadgeLabel: FC<IBadgeLabel> = ({ children, styleOverwrite }) => {
-  return <SBadgeLabel styleOverwrite={styleOverwrite}>{children}</SBadgeLabel>;
+const BadgeLabel: FC<IBadgeLabel> = ({ value, styleOverwrite }) => {
+  return <SBadgeLabel styleOverwrite={styleOverwrite}>{value}</SBadgeLabel>;
 };
 
 export default BadgeLabel;
