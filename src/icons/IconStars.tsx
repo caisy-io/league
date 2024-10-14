2416b878e8a3797404f892a00142bada3e1109ab
Ahmed Ghazy <ahmed.h.ghazy3@gmail.com>
2024-10-14 16:20:24 +0300

    fix: :bug: adding IconBase

--- src/icons/IconStars.tsx
+++ src/icons/IconStars.tsx
@@ -1,209 +1,154 @@
 import { FC } from "react";
+import { SIconBase } from "./styles/SIconBase";
 
 const IconSize = {
   16: (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-      <path d="M4.00002 11.3333V14" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path
-        d="M2.66669 12.6667H5.33335"
-        stroke="#323232"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path d="M11.3333 2V4.66667" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M10 3.33333H12.6667" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <SIconBase xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+      <path d="M4.00002 11.3333V14" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M2.66669 12.6667H5.33335" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M11.3333 2V4.66667" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M10 3.33333H12.6667" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
       <path
         d="M4.33331 3.33333C4.33331 4.622 5.37798 5.66667 6.66665 5.66667"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M6.66665 5.66667C5.37798 5.66667 4.33331 6.71133 4.33331 8"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M4.33333 8C4.33333 6.71133 3.28867 5.66667 2 5.66667"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M2 5.66667C3.28867 5.66667 4.33333 4.622 4.33333 3.33333"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M7.73932 10.5187C9.46799 10.5187 10.8693 9.11733 10.8693 7.38867"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M10.87 7.38867C10.87 9.11733 12.2713 10.5187 14 10.5187"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M14 10.5187C12.2713 10.5187 10.87 11.92 10.87 13.6487"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M10.87 13.6487C10.87 11.92 9.46866 10.5187 7.73999 10.5187"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </SIconBase>
   ),
   20: (
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-      <path d="M4.99998 14.1667V17.5" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path
-        d="M3.33334 15.8333H6.66668"
-        stroke="#323232"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path d="M14.1667 2.5V5.83333" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M12.5 4.16667H15.8333" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <SIconBase xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path d="M4.99998 14.1667V17.5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M3.33334 15.8333H6.66668" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M14.1667 2.5V5.83333" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M12.5 4.16667H15.8333" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
       <path
         d="M5.41669 4.16667C5.41669 5.7775 6.72252 7.08333 8.33335 7.08333"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M8.33335 7.08333C6.72252 7.08333 5.41669 8.38917 5.41669 10"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M5.41667 10C5.41667 8.38917 4.11083 7.08333 2.5 7.08333"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M2.5 7.08333C4.11083 7.08333 5.41667 5.7775 5.41667 4.16667"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M9.67419 13.1483C11.835 13.1483 13.5867 11.3967 13.5867 9.23583"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M13.5875 9.23583C13.5875 11.3967 15.3392 13.1483 17.5 13.1483"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M17.5 13.1483C15.3392 13.1483 13.5875 14.9 13.5875 17.0608"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M13.5875 17.0608C13.5875 14.9 11.8358 13.1483 9.67499 13.1483"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </SIconBase>
   ),
   24: (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-      <path d="M6 17V21" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M4 19H8" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M17 3V7" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M15 5H19" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path
-        d="M6.5 5C6.5 6.933 8.067 8.5 10 8.5"
-        stroke="#323232"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M10 8.5C8.067 8.5 6.5 10.067 6.5 12"
-        stroke="#323232"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M6.5 12C6.5 10.067 4.933 8.5 3 8.5"
-        stroke="#323232"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M3 8.5C4.933 8.5 6.5 6.933 6.5 5"
-        stroke="#323232"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+    <SIconBase xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M6 17V21" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M4 19H8" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M17 3V7" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M15 5H19" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M6.5 5C6.5 6.933 8.067 8.5 10 8.5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M10 8.5C8.067 8.5 6.5 10.067 6.5 12" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M6.5 12C6.5 10.067 4.933 8.5 3 8.5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M3 8.5C4.933 8.5 6.5 6.933 6.5 5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
       <path
         d="M11.609 15.778C14.202 15.778 16.304 13.676 16.304 11.083"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M16.305 11.083C16.305 13.676 18.407 15.778 21 15.778"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M21 15.778C18.407 15.778 16.305 17.88 16.305 20.473"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M16.305 20.473C16.305 17.88 14.203 15.778 11.61 15.778"
-        stroke="#323232"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </SIconBase>
   ),
 };
 
